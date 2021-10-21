ce185c681d2af315d03c0d2a8a05b3f64181b9cf
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-21 10:50:43 +0200

    Elimina los comentarios innecesarios y cambia el valor variable x a 2

--- src/main/java/ejerciciosbucles/Ej1.java
+++ src/main/java/ejerciciosbucles/Ej1.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejerciciosbucles;
 
-/**
- *
- * @author samjimmaz
- */
 public class Ej1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        int x = 2;
+
+        while (x <= 10) {
+            System.out.print(x);
+            System.out.println(" - ");
+            x = x + 1;
+        }
+
     }
-    
+
 }
