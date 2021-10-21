b2012000b2169bd785f2a51ac3ec21214eb3eff1
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-21 11:11:52 +0200

    Añade comentarios explicando el código y corrije errores de escritura

--- src/main/java/ejerciciosbucles/Ej1.java
+++ src/main/java/ejerciciosbucles/Ej1.java
@@ -5,13 +5,15 @@ public class Ej1 {
     public static void main(String[] args) {
 
         int x = 2;
-
-        while (x <= 10) {
+        // Este bucle imprime x, imprime un guión y le suma 1 a x mientras que x sea menor que 100
+        //Se detiene cuando x valga 101
+        while (x <= 100) {
             System.out.print(x);
-            System.out.println(" - ");
-            x = x + 1;
+            System.out.print(" - ");
+            x = x + 1; // x++; ++x; x+=1;
         }
-
+        System.out.println("");
+        System.out.println("x vale: " + x);
     }
 
 }
