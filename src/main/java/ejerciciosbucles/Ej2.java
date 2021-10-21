d10e90bcccf2a5e434c4ad9a1deec5caddf36f2b
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-21 11:58:01 +0200

    Borra los comentarios innecesarios y pone formato

--- src/main/java/ejerciciosbucles/Ej2.java
+++ src/main/java/ejerciciosbucles/Ej2.java
@@ -4,17 +4,22 @@
  */
 package ejerciciosbucles;
 
-/**
- *
- * @author samjimmaz
- */
+import java.util.Scanner;
+
 public class Ej2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        int n, x;
+        System.out.print("Ingrese el valor final:");
+        n = teclado.nextInt();
+        x = 1;
+        while (x <= n) {
+            System.out.print(x);
+            System.out.print(" - ");
+            x = x + 1;
+        }
+
     }
-    
+
 }
