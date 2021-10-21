2103b206356fcc8eb8597f2ea1f99f90e505c461
sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-21 18:21:14 +0200

    Añade comentarios sobre el por qué de lo que hizo

--- src/main/java/ejerciciosbucles/Ej3.java
+++ src/main/java/ejerciciosbucles/Ej3.java
@@ -7,7 +7,8 @@ public class Ej3 {
     
     public static void main(String[] args) {
        int x = 15;
-
+// Usamos la potencia de 2 elevada a 24 ya que no podemos olvidar que el primer número es 15 por 2 elevado a 0, lo cual si hacemos
+// a potencia de 25 estaríamos imprimiendo 26 número cuando piden 25
         while (x <= 15*Math.pow(2, 24)) {
             System.out.print(x);
             System.out.print(" - ");
