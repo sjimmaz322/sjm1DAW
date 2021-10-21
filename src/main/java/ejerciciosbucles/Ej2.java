4e7f6ae193c5368c83be6c9bb60e7a03cd4f39e5
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-21 12:01:37 +0200

    Añade comentarios sobre el funcionamiento del programa y su función

--- src/main/java/ejerciciosbucles/Ej2.java
+++ src/main/java/ejerciciosbucles/Ej2.java
@@ -9,11 +9,17 @@ import java.util.Scanner;
 public class Ej2 {
 
     public static void main(String[] args) {
+        //Ponemos la el escanner que nos permita introducir una variable por teclado
         Scanner teclado = new Scanner(System.in);
+        //Declaramos las variables int necesarias
         int n, x;
+        //Imprimimos por consola un mensaje que nos pide introducir el valor de n
         System.out.print("Ingrese el valor final:");
+        //Inicializamos n con el valor introducido
         n = teclado.nextInt();
+        //Inicializamos la variable x
         x = 1;
+        //Utilizamos el bucle while para que se vaya sumando 1 a x hasta que los valores de x y n sean iguales
         while (x <= n) {
             System.out.print(x);
             System.out.print(" - ");
