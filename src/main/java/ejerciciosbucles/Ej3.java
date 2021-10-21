c3b6ea6137b8437c6af552ea8b6f463f4abc8d39
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-21 12:43:03 +0200

    Cambia la variable x de double a int para que el resultado sea un número entero

--- src/main/java/ejerciciosbucles/Ej3.java
+++ src/main/java/ejerciciosbucles/Ej3.java
@@ -6,7 +6,7 @@ public class Ej3 {
 
     
     public static void main(String[] args) {
-       double x = 15;
+       int x = 15;
 
         while (x <= 15*Math.pow(2, 24)) {
             System.out.print(x);
