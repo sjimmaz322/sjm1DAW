a1c450c01c1033e3821a036dcaa9b669edbc731a
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-29 13:28:54 +0200

    Mejorada la legibilidad del programa añadiendo saltos de línea

--- src/main/java/ejercicioscasa/EjercicioFactorialFor.java
+++ src/main/java/ejercicioscasa/EjercicioFactorialFor.java
@@ -6,10 +6,14 @@ public class EjercicioFactorialFor {
 
   
     public static void main(String[] args) {
+        
         Scanner entradaTeclado = new Scanner(System.in);
+        
         System.out.println("Introduce el numero al cual quieras calcuar el factorial");
         int n = entradaTeclado.nextInt();
+        
         int i, factorial=1;
+        
         for (i =1; i<=n; i++ ){
             factorial=factorial*i;
         }
