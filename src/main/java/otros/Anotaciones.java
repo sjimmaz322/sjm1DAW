728c410923c668d0c5834af5c6c9f5442464804b
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-10-29 13:19:17 +0200

    Añadido ejemplo para que se vea funcionamiento

--- src/main/java/otros/Anotaciones.java
+++ src/main/java/otros/Anotaciones.java
@@ -1,17 +1,22 @@
-
 package otros;
 
+import java.util.Random;
 
 public class Anotaciones {
 
-    
     public static void main(String[] args) {
-     //Número aleatorio entre dos números
-     //Número mayor nMayor
-     //Número menor nMenor
-     //Rango entre nMayor y nMenor
-     // int n = numAleatorio.nextInt (nMayor -nMenor +1) +nMenor;
-     //Ojo con los signos
+        //Número aleatorio entre dos números
+        //Número mayor nMayor
+        //Número menor nMenor
+        //Rango entre nMayor y nMenor
+        // int n = numAleatorio.nextInt (nMayor -nMenor +1) +nMenor;
+        //Ojo con los signos
+        //ejemplo entre 36 y 12
+        Random numAleatorio = new Random();
+
+        int n = numAleatorio.nextInt(36 - 12 + 1) + 12;
+        System.out.println(n);
+
     }
-    
+
 }
