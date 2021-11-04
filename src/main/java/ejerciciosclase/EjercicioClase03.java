8c8c2104367dd0e8c44aa8fc754518c9800ec451
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-04 12:47:11 +0100

    Creada la excepción para el caso de introducir una letra

--- src/main/java/ejerciciosclase/EjercicioClase03.java
+++ src/main/java/ejerciciosclase/EjercicioClase03.java
@@ -1,24 +1,33 @@
 package ejerciciosclase;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class EjercicioClase03 {
 
     public static void main(String[] args) {
             
-        int numero1, numero2;
+        int numero1=1, numero2=1;
         
         Scanner teclado = new Scanner(System.in);
         
+        try{
+            
         System.out.println("Introduce el primer número: ");
         numero1 = teclado.nextInt();
         
         System.out.println("Introduce el segundo número: ");
         numero2 = teclado.nextInt();
         
+        } catch(InputMismatchException ime){
+            System.out.println("Números, no letras por favor");
+        }
+        
+        
         int division = numero1/numero2; 
         
         System.out.println("Resultado: " + division);
+        
     }
 
 }
