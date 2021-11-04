81c0c103127d7203c3da11346edae338da671eac
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-04 10:14:44 +0100

    Cambio orden suma ronda ganada y corrección rango clase random

--- src/main/java/ejerciciosclase/EjercicioClase02.java
+++ src/main/java/ejerciciosclase/EjercicioClase02.java
@@ -7,7 +7,6 @@ Si los números pensados (suma) <7 gana el jugador 1, si es >7 gana el jugador 2
 Si la suma ==7 no gana ninguno.
 Decir el mejor de 5 rondas.
  */
-
 import java.util.Random;
 import javax.swing.JOptionPane;
 
@@ -32,59 +31,60 @@ public class EjercicioClase02 {
             //Generamos los número aleatorio de ambos jugadores
             Random numAleatorio = new Random();
 
-            numJ1 = numAleatorio.nextInt(4) + 1;
-            numJ2 = numAleatorio.nextInt(4) + 1;
+            numJ1 = numAleatorio.nextInt(5) + 1;
+            numJ2 = numAleatorio.nextInt(5) + 1;
 
             //Inicializamos la variable de la suma de ambos números
             sumaNum = numJ1 + numJ2;
 
             //Condición para una ronda nula
             if (sumaNum == 7) {
-                
-                JOptionPane.showMessageDialog(null, "Ronda " + numRonda + "\n" + "Ronda no válida, no hay ganadores" + "\n"+
-                        "El jugador 1 lleva " + victoriasJ1 + " rondas ganadas" + "\n" +
-                        "El jugador 2 lleva " +  victoriasJ2 + " rondas ganadas");
-            //Condición para la victoria del jugador 1    
+
+                JOptionPane.showMessageDialog(null, "Ronda " + numRonda + "\n" + "Ronda no válida, no hay ganadores" + "\n"
+                        + "El jugador 1 lleva " + victoriasJ1 + " rondas ganadas" + "\n"
+                        + "El jugador 2 lleva " + victoriasJ2 + " rondas ganadas");
+                //Condición para la victoria del jugador 1    
             } else if (sumaNum < 7) {
-                
-                JOptionPane.showMessageDialog(null, "Ronda " + numRonda + " ganada por el Jugador 1" + "\n" +
-                        "El jugador 1 obtuvo un " + numJ1
-                        + "\n" + "El jugador 2 obtuvo un: " + numJ2 + "\n"+
-                        "El jugador 1 lleva " + victoriasJ1 + " rondas ganadas" + "\n" +
-                        "El jugador 2 lleva " +  victoriasJ2 + " rondas ganadas");
-                
-                //Aumento del número de ronda
-                numRonda = numRonda + 1;
                 //Aumento de las victorias del jugador 1
                 victoriasJ1 = victoriasJ1 + 1;
-                
-            //Condición para la victoria del jugador 2    
-            } else if (sumaNum > 7) {
-                
-                JOptionPane.showMessageDialog(null, "Ronda " + numRonda + " ganada por el jugador 2" + "\n" +
-                        "El jugador 1 obtuvo un " + numJ1
-                        + "\n" + "El jugador 2 obtuvo un: " + numJ2 + "\n" +
-                        "El jugador 1 lleva " + victoriasJ1 + " rondas ganadas" + "\n" +
-                        "El jugador 2 lleva " +  victoriasJ2 + " rondas ganadas");
-                
-                //Aumento de ronda
+
+                JOptionPane.showMessageDialog(null, "Ronda " + numRonda + " ganada por el Jugador 1" + "\n"
+                        + "El jugador 1 obtuvo un " + numJ1
+                        + "\n" + "El jugador 2 obtuvo un: " + numJ2 + "\n"
+                        + "El jugador 1 lleva " + victoriasJ1 + " rondas ganadas" + "\n"
+                        + "El jugador 2 lleva " + victoriasJ2 + " rondas ganadas");
+
+                //Aumento del número de ronda
                 numRonda = numRonda + 1;
+
+                //Condición para la victoria del jugador 2    
+            } else if (sumaNum > 7) {
                 //Aumento de las victorias del jugador 2
                 victoriasJ2 = victoriasJ2 + 1;
+
+                JOptionPane.showMessageDialog(null, "Ronda " + numRonda + " ganada por el jugador 2" + "\n"
+                        + "El jugador 1 obtuvo un " + numJ1
+                        + "\n" + "El jugador 2 obtuvo un: " + numJ2 + "\n"
+                        + "El jugador 1 lleva " + victoriasJ1 + " rondas ganadas" + "\n"
+                        + "El jugador 2 lleva " + victoriasJ2 + " rondas ganadas");
+
+                //Aumento de ronda
+                numRonda = numRonda + 1;
+
             }
-        //Condición para la repetición del bucle
+            //Condición para la repetición del bucle
         } while ((victoriasJ1 < LIMITE_VICTORIAS && victoriasJ2 < LIMITE_VICTORIAS));
-        
+
         //Si el jugador 1 ha alcanzado el número de rondas ganadas necesarias para ganar el juego
-        if (victoriasJ1==LIMITE_VICTORIAS){
-            
-            JOptionPane.showMessageDialog(null, "El ganador ha sido el Jugador 1, ha ganado 3 rondas"+"\n"+
-                    "El jugador 2 obtuvo: " + victoriasJ2 + " victorias");
-        //Si el jugador 2 ha alcanzado el número de rondas ganadas necesarias para ganar el juego    
+        if (victoriasJ1 == LIMITE_VICTORIAS) {
+
+            JOptionPane.showMessageDialog(null, "El ganador ha sido el Jugador 1, ha ganado 3 rondas" + "\n"
+                    + "El jugador 2 obtuvo: " + victoriasJ2 + " victorias");
+            //Si el jugador 2 ha alcanzado el número de rondas ganadas necesarias para ganar el juego    
         } else {
-            
-            JOptionPane.showMessageDialog(null, "El ganador ha sido el Jugador 2, ha ganado 3 rondas"+"\n"+
-                    "El jugador 2 obtuvo: " + victoriasJ1 + " victorias");
+
+            JOptionPane.showMessageDialog(null, "El ganador ha sido el Jugador 2, ha ganado 3 rondas" + "\n"
+                    + "El jugador 2 obtuvo: " + victoriasJ1 + " victorias");
         }
     }
 }
