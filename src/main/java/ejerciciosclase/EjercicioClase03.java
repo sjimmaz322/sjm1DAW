7f8dfeabcc09422166629ef0acc9e5fb3ddcedb3
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-05 13:11:14 +0100

    Añadida excepción división por cero

--- src/main/java/ejerciciosclase/EjercicioClase03.java
+++ src/main/java/ejerciciosclase/EjercicioClase03.java
@@ -6,37 +6,40 @@ import java.util.Scanner;
 public class EjercicioClase03 {
 
     public static void main(String[] args) {
-            
-        int numero1=1, numero2=1;
-        
+
+        int numero1 = 1, numero2 = 1, division = 0;
+
         Scanner teclado = new Scanner(System.in);
-        
+
         boolean seguir = true;
-        
+
         do {
-        try{
-            
-        System.out.println("Introduce el primer número: ");
-        numero1 = teclado.nextInt();
-        
-        System.out.println("Introduce el segundo número: ");
-        numero2 = teclado.nextInt();
-        
-        seguir = false;
-        
-        } catch(InputMismatchException ime){
-            
-            System.out.println("Números, no letras por favor. "+
-                    "Introduce solo números");
-            teclado.nextLine();
-             
-        } 
+            try {
+
+                System.out.println("Introduce el primer número: ");
+                numero1 = teclado.nextInt();
+
+                System.out.println("Introduce el segundo número: ");
+                numero2 = teclado.nextInt();
+
+                division = numero1 / numero2;
+
+                seguir = false;
+
+            } catch (InputMismatchException ime) {
+
+                System.out.println("Números, no letras por favor. "
+                        + "Introduce solo números");
+                teclado.nextLine();
+
+            } catch (ArithmeticException ae) {
+                System.out.println("No se puede dividir por cero");
+            }
+
         } while (seguir);
-        
-        int division = numero1/numero2; 
-        
+
         System.out.println("Resultado: " + division);
-        
+
     }
 
 }
