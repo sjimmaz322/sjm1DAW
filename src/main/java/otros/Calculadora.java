0318a036864a9e086e927d28b84b8982db52c964
sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-09 22:44:46 +0100

    Added comments

--- src/main/java/otros/Calculadora.java
+++ src/main/java/otros/Calculadora.java
@@ -35,18 +35,18 @@ public class Calculadora {
 
                     solicitarDatos = false;
 
-                } catch (InputMismatchException ime) {
+                } catch (InputMismatchException ime) { 
                     // Código para tratar el error    
                     System.out.println("Se ha introducido texto en lugar de números. "
                             + "Vuelva a introducir los datos.");
                     sc.nextLine();
                 }
-            } while (solicitarDatos);
-        } while (!((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100)));
+            } while (solicitarDatos);//Condición de repetición del error
+        } while (!((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100))); //Condición de repeticición de rango incorrecto
         do {
             do {
                 do {
-                    try {
+                    try {//Bloque candidato a error
                         System.out.println("Elija la operación que desea calcular");
                         System.out.println("1 para sumar");
                         System.out.println("2 para restar");
@@ -56,12 +56,12 @@ public class Calculadora {
                         select = sc.nextInt();
                         solicitarDatosMenu = false;
 
-                    } catch (InputMismatchException ime) {
+                    } catch (InputMismatchException ime) {//Mensaje de error
                         System.out.println("Introduciendo letras en vez de número otra vez... inténtalo de nuevo");
                         sc.nextLine();
                     }
-                } while (solicitarDatosMenu);
-            } while (!((select == 1) || (select == 2) || (select == 3) || (select == 4) || (select == 0)));
+                } while (solicitarDatosMenu);//Condición de repetición por error
+            } while (!((select == 1) || (select == 2) || (select == 3) || (select == 4) || (select == 0)));//Condición de repetición por elección fuera de opciones
 
             switch (select) {
                 case 1:
@@ -81,10 +81,10 @@ public class Calculadora {
                     break;
                 case 4:
                     System.out.println("Ha decidido dividir ambos operandos");
-                    try {
+                    try {//Bloque candidato a lanzar excepción
                         division = (double) operando1 / (double) operando2;
                         System.out.println("El resultado es: " + division);
-                    } catch (ArithmeticException ae) {
+                    } catch (ArithmeticException ae) {//Mensaje de error
                         System.out.println("No se puede dividir por cero");
                     }
                     break;
@@ -92,7 +92,7 @@ public class Calculadora {
                     System.out.println("Ha decidido sabiamente, cerrando el programa...");
                     break;
             }
-        } while (select != 0);
+        } while (select != 0);//Condición para finalizar el programa
     }
 
 }
