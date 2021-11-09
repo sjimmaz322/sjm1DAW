3658f8633310a7b0e18cb0686cdc39c58bd92fa3
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-09 14:28:02 +0100

    Creación de pedir números y excepción

--- src/main/java/otros/Calculadora.java
+++ src/main/java/otros/Calculadora.java
@@ -1,5 +1,8 @@
 package otros;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Calculadora {
 
     public static void main(String[] args) {
@@ -8,6 +11,31 @@ public class Calculadora {
         // Filtramos entrada de usuario
         // Controlar las excepciones ante error de usuario
         // Cuatro opciones: Sumar, restar, multiplicar y dividir.
+        Scanner sc = new Scanner(System.in);
+        int operando1, operando2;
+        boolean solicitarDatos = true;
+        
+        System.out.println("Bienvenido a la Calculadora");
+        System.out.println("Introduce dos operandos");
+        do {
+        try {
+        // Bloque candidato a lanzar excepción    
+        System.out.println("Introduce operando 1:");
+        operando1 = sc.nextInt();
+        
+        System.out.println("Introduce operando 2:");
+        operando2 = sc.nextInt();
+        
+        solicitarDatos = false;
+        
+        }catch (InputMismatchException ime){
+        // Código para tratar el error    
+        System.out.println("Se ha introducido texto en lugar de números. "
+                + "Vuelva a introducir los datos.");   
+        sc.next();
+        }
+        }while (solicitarDatos);
+        
     }
 
 }
