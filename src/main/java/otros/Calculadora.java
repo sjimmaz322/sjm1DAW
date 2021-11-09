07271bc9cd14b2d40a43aa3f9527db02d5b6285f
sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-09 22:40:56 +0100

    Formateado y adición de comentarios

--- src/main/java/otros/Calculadora.java
+++ src/main/java/otros/Calculadora.java
@@ -11,8 +11,9 @@ public class Calculadora {
         // Filtramos entrada de usuario
         // Controlar las excepciones ante error de usuario
         // Cuatro opciones: Sumar, restar, multiplicar y dividir.
-        
-        Scanner sc = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in); // Clase Scanner
+        //Definición e inicialización de variables necesarias
         int operando1 = 0, operando2 = 0, select = 0;
         boolean solicitarDatos = true, solicitarDatosMenu = true;
         int suma, resta, multiplication;
@@ -42,56 +43,56 @@ public class Calculadora {
                 }
             } while (solicitarDatos);
         } while (!((operando1 > -100 && operando1 < 100) && (operando2 > -100 && operando2 < 100)));
-        do{
         do {
             do {
-                try {
-                    System.out.println("Elija la operación que desea calcular");
-                    System.out.println("1 para sumar");
-                    System.out.println("2 para restar");
-                    System.out.println("3 para multiplicar");
-                    System.out.println("4 para dividir");
-                    System.out.println("0 para cancelar");
-                    select = sc.nextInt();
-                    solicitarDatosMenu = false;
+                do {
+                    try {
+                        System.out.println("Elija la operación que desea calcular");
+                        System.out.println("1 para sumar");
+                        System.out.println("2 para restar");
+                        System.out.println("3 para multiplicar");
+                        System.out.println("4 para dividir");
+                        System.out.println("0 para cancelar");
+                        select = sc.nextInt();
+                        solicitarDatosMenu = false;
 
-                } catch (InputMismatchException ime) {
-                    System.out.println("Introduciendo letras en vez de número otra vez... inténtalo de nuevo");
-                    sc.nextLine();
-                }
-            } while (solicitarDatosMenu);
-        } while (!((select == 1) || (select == 2) || (select == 3) || (select == 4) || (select == 0)));
+                    } catch (InputMismatchException ime) {
+                        System.out.println("Introduciendo letras en vez de número otra vez... inténtalo de nuevo");
+                        sc.nextLine();
+                    }
+                } while (solicitarDatosMenu);
+            } while (!((select == 1) || (select == 2) || (select == 3) || (select == 4) || (select == 0)));
 
-        switch (select) {
-            case 1:
-                System.out.println("Ha elegido sumar ambos operandos");
-                suma = operando1 + operando2;
-                System.out.println("El resultado es: " + suma);
-                break;
-            case 2:
-                System.out.println("Ha elegido restar ambos operandos");
-                resta = operando1 - operando2;
-                System.out.println("El resultado es: " + resta);
-                break;
-            case 3:
-                System.out.println("Ha decidido multiplicar ambos operandos");
-                multiplication = operando1 * operando2;
-                System.out.println("El resultado es: " + multiplication);
-                break;
-            case 4:
-                System.out.println("Ha decidido dividir ambos operandos");
-                try {
-                    division = (double)operando1 / (double)operando2;
-                    System.out.println("El resultado es: " + division);
-                } catch (ArithmeticException ae) {
-                    System.out.println("No se puede dividir por cero");
-                }
-                break;
-            case 0:
-                System.out.println("Ha decidido sabiamente, cerrando el programa...");
-                break;
-        }
-    } while (select!=0);
-        }
+            switch (select) {
+                case 1:
+                    System.out.println("Ha elegido sumar ambos operandos");
+                    suma = operando1 + operando2;
+                    System.out.println("El resultado es: " + suma);
+                    break;
+                case 2:
+                    System.out.println("Ha elegido restar ambos operandos");
+                    resta = operando1 - operando2;
+                    System.out.println("El resultado es: " + resta);
+                    break;
+                case 3:
+                    System.out.println("Ha decidido multiplicar ambos operandos");
+                    multiplication = operando1 * operando2;
+                    System.out.println("El resultado es: " + multiplication);
+                    break;
+                case 4:
+                    System.out.println("Ha decidido dividir ambos operandos");
+                    try {
+                        division = (double) operando1 / (double) operando2;
+                        System.out.println("El resultado es: " + division);
+                    } catch (ArithmeticException ae) {
+                        System.out.println("No se puede dividir por cero");
+                    }
+                    break;
+                case 0:
+                    System.out.println("Ha decidido sabiamente, cerrando el programa...");
+                    break;
+            }
+        } while (select != 0);
+    }
 
 }
