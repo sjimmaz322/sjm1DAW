deb68ebb735994578331915a1f1306f430b1fb26
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-09 14:18:32 +0100

    Añadidos requisitos de programa

--- src/main/java/otros/Calculadora.java
+++ src/main/java/otros/Calculadora.java
@@ -3,7 +3,11 @@ package otros;
 public class Calculadora {
 
     public static void main(String[] args) {
-
+        // Scanner
+        // Mostrar menú opciones
+        // Filtramos entrada de usuario
+        // Controlar las excepciones ante error de usuario
+        // Cuatro opciones: Sumar, restar, multiplicar y dividir.
     }
 
 }
