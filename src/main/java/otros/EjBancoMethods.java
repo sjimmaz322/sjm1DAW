026f412e1325151a896c537e183ecad6194587f1
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-11 12:34:38 +0100

    Sacado JOption de los métodos e introducido en el método main

--- src/main/java/otros/EjBancoMethods.java
+++ src/main/java/otros/EjBancoMethods.java
@@ -3,66 +3,45 @@ package otros;
 import javax.swing.JOptionPane;
 
 public class EjBancoMethods {
-    private int age;
-    
-    private static boolean filtrarEdad(boolean a) {
-        final int MIN_AGE = 18, MAX_AGE = 65;
-        int age;
+
+    // private int age;
+    private static boolean filtrarEdad(boolean controlEdad) {
         
-            String edadTexto = JOptionPane.showInputDialog("Introduzca su edad");
-            age = Integer.parseInt(edadTexto);
-            
-            if (age > MIN_AGE && age < MAX_AGE) {
-                return true;
-            }
-            else{
-                return false;
-            }
+        final int MIN_AGE = 18, MAX_AGE = 65;
+        int age = 0;
+
+        if (age > MIN_AGE && age < MAX_AGE) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-        
-    
+    private static boolean filtrarPrestamo(boolean controlMonetario) {
 
-    private static boolean filtrarPrestamo(boolean a) {
-        
         int salary = 0, moneyAsked = 0;
         final int MONTHS_IN_YEAR = 12;
         final double MULTIPLY_FACTOR = 5.85;
         double creditLimit = 0;
 
-        String salarioTexto = JOptionPane.showInputDialog("Introduzca su salario bruto mensual");
-        salary = Integer.parseInt(salarioTexto);
-
-        creditLimit = (salary * MONTHS_IN_YEAR * MULTIPLY_FACTOR);
-
-        
-            String cantidadPrestamoTexto = JOptionPane.showInputDialog("Introduzca el dinero que desea solicitar");
-            moneyAsked = Integer.parseInt(cantidadPrestamoTexto);
-            
-            if (moneyAsked <= creditLimit) {
-                return true;
-            }
-            else {
-                return false;
-            }
+        if (moneyAsked <= creditLimit) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    private static boolean filtrarDevolucion(boolean a) {
-        
+    private static boolean filtrarDevolucion(boolean controlDevolucion) {
+
         int deadlines = 0;
         int age = 0;
         final int MIN_DEADLINES = 5, MAX_DEADLINES = 30, LIMIT_DEADLINES = 75;
 
-
-            String plazosTexto = JOptionPane.showInputDialog("Introduzca los años en los que desea devolver el préstamo");
-            deadlines = Integer.parseInt(plazosTexto);
-            
-            if ((deadlines < MAX_DEADLINES) && (deadlines > MIN_DEADLINES) && (age + deadlines < LIMIT_DEADLINES)) {
-                return true;
-            }
-            else {
-                return false;
-        }  
+        if ((deadlines < MAX_DEADLINES) && (deadlines > MIN_DEADLINES) && (age + deadlines < LIMIT_DEADLINES)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args) {
@@ -77,19 +56,38 @@ public class EjBancoMethods {
         Método que diga si los años de devolución son correctos o no.
         Método recibe años a devolver y devuelve resultado (Boolean). (Return)
          */
-
-
+        final int MIN_AGE = 18, MAX_AGE = 65;
+        int age;
+        int deadlines = 0;
+        final int MIN_DEADLINES = 5, MAX_DEADLINES = 30, LIMIT_DEADLINES = 75;
+        int salary = 0, moneyAsked = 0;
+        final int MONTHS_IN_YEAR = 12;
+        final double MULTIPLY_FACTOR = 5.85;
+        double creditLimit = 0;
         boolean seguir0 = false, seguir1 = false, seguir2 = false, seguir3 = false;
 
+        String edadTexto = JOptionPane.showInputDialog("Introduzca su edad");
+        age = Integer.parseInt(edadTexto);
+
+        String salarioTexto = JOptionPane.showInputDialog("Introduzca su salario bruto mensual");
+        salary = Integer.parseInt(salarioTexto);
+
+        creditLimit = (salary * MONTHS_IN_YEAR * MULTIPLY_FACTOR);
+
+        String cantidadPrestamoTexto = JOptionPane.showInputDialog("Introduzca el dinero que desea solicitar");
+        moneyAsked = Integer.parseInt(cantidadPrestamoTexto);
+
+        String plazosTexto = JOptionPane.showInputDialog("Introduzca los años en los que desea devolver el préstamo");
+        deadlines = Integer.parseInt(plazosTexto);
+        
 
         seguir1 = filtrarEdad(seguir0);
         seguir2 = filtrarPrestamo(seguir0);
         seguir3 = filtrarDevolucion(seguir0);
 
-        if ((seguir1==true && seguir2==true && seguir3==true)){
+        if ((seguir1 == true && seguir2 == true && seguir3 == true)) {
             JOptionPane.showMessageDialog(null, "Solicitud Aprobada");
-        }
-        else {
+        } else {
             JOptionPane.showMessageDialog(null, "Solicitud Denegada");
         }
     }
