17e4b5253c7144e978c7ae2d6376b308d79e0e06
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-11 11:53:18 +0100

    Finalizado el programa, pseudo-testado, no comentado

--- src/main/java/otros/EjBancoMethods.java
+++ src/main/java/otros/EjBancoMethods.java
@@ -4,6 +4,66 @@ import javax.swing.JOptionPane;
 
 public class EjBancoMethods {
 
+    private static boolean filtrarEdad(boolean a) {
+        final int MIN_AGE = 18, MAX_AGE = 65;
+        int age = 0;
+        do {
+            String edadTexto = JOptionPane.showInputDialog("Introduzca su edad" + "\n"
+                    + "Si los datos son incorrectos se pedirán de nuevo");
+            age = Integer.parseInt(edadTexto);
+            if (age < MIN_AGE || age > MAX_AGE) {
+                JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
+                        + "Rango de edad permitido: 18 - 65 años");
+            }
+        } while (age < MIN_AGE || age > MAX_AGE);
+
+        return (true);
+    }
+
+    private static boolean filtrarPrestamo(boolean a) {
+        int salary = 0, moneyAsked = 0;
+        final int MONTHS_IN_YEAR = 12;
+        final double MULTIPLY_FACTOR = 5.85;
+        double creditLimit = 0;
+
+        String salarioTexto = JOptionPane.showInputDialog("Introduzca su salario bruto mensual");
+        salary = Integer.parseInt(salarioTexto);
+
+        creditLimit = (salary * MONTHS_IN_YEAR * MULTIPLY_FACTOR);
+
+        do {
+            String cantidadPrestamoTexto = JOptionPane.showInputDialog("Introduzca el dinero que desea solicitar" + "\n"
+                    + "Si los datos son incorrectos se pedirán de nuevo");
+            moneyAsked = Integer.parseInt(cantidadPrestamoTexto);
+            if (moneyAsked > creditLimit) {
+                JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
+                        + "El máximo solicitable será el salario anual multiplicado por el factor multiplicativo (5.85)");
+            }
+        } while (moneyAsked > creditLimit);
+        return (true);
+    }
+
+    private static boolean filtrarDevolucion(boolean a) {
+        int deadlines = 0, age= 0;
+        final int MIN_DEADLINES = 5, MAX_DEADLINES = 30, LIMIT_DEADLINES = 75;
+
+        do {
+            String edadTexto = JOptionPane.showInputDialog("Introduzca su edad de nuevo");
+            age = Integer.parseInt(edadTexto);
+            String plazosTexto = JOptionPane.showInputDialog("Introduzca los años en los que desea devolver el préstamo" + "\n"
+                    + "Si los datos son incorrectos se pedirán de nuevo");
+            deadlines = Integer.parseInt(plazosTexto);
+            if ((deadlines > MAX_DEADLINES) || (deadlines < MIN_DEADLINES) || (age + deadlines > LIMIT_DEADLINES)) {
+                JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
+                        + "Años de devolución minimos: 5" + "\n"
+                        + "Años de devolución máximos: 30" + "\n"
+                        + "Atención: Los años en los que devuelva el préstamo sumados a su edad no podrá ser superior al límite permitido."
+                        + "\n" + "Límite permitido: 75 años.");
+            }
+        } while ((deadlines > MAX_DEADLINES) || (deadlines < MIN_DEADLINES) || (age + deadlines > LIMIT_DEADLINES));
+        return (true);
+    }
+
     public static void main(String[] args) {
         /*
         1-
@@ -16,64 +76,21 @@ public class EjBancoMethods {
         Método que diga si los años de devolución son correctos o no.
         Método recibe años a devolver y devuelve resultado (Boolean). (Return)
          */
-        
-        int edad;
-        int salario;
-        double limiteCredito;
-        int cantidadPrestamo;
-        int plazos;
-        int ejecucion;
-        final double FACTOR_MULTIPLICATIVO = 5.85;
-        final int MESES_ANUALIDAD = 12;
-        final int EDAD_MINIMA = 18;
-        final int EDAD_MAXIMA = 65;
-        final int TOPE_DEVOLUCION = 75;
-        final int PLAZO_MINIMO = 5;
-        final int PLAZO_MAXIMO = 30;
 
-        do {
-            String ejecucionTexto = JOptionPane.showInputDialog("¿Desea ejecutar el programa?" + "\n" + "Introduzca 1 para si" + "\n"
-                    + "Introduzca 0 para no");
-            ejecucion = Integer.parseInt(ejecucionTexto);
-            if (ejecucion == 1) {
-                do {
-                    String edadTexto = JOptionPane.showInputDialog("Introduzca su edad" + "\n"
-                            + "Si los datos son incorrectos se pedirán de nuevo");
-                    edad = Integer.parseInt(edadTexto);
-                    if (edad < EDAD_MINIMA || edad > EDAD_MAXIMA) {
-                        JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
-                                + "Rango de edad permitido: 18 - 65 años");
-                    }
-                } while (edad < EDAD_MINIMA || edad > EDAD_MAXIMA);
-                String salarioTexto = JOptionPane.showInputDialog("Introduzca su salario bruto mensual");
-                salario = Integer.parseInt(salarioTexto);
-                limiteCredito = (salario * MESES_ANUALIDAD * FACTOR_MULTIPLICATIVO);
-                do {
-                    String cantidadPrestamoTexto = JOptionPane.showInputDialog("Introduzca el dinero que desea solicitar" + "\n"
-                            + "Si los datos son incorrectos se pedirán de nuevo");
-                    cantidadPrestamo = Integer.parseInt(cantidadPrestamoTexto);
-                    if (cantidadPrestamo > limiteCredito) {
-                        JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
-                                + "El máximo solicitable será el salario anual multiplicado por el factor multiplicativo (5.85)");
-                    }
-                } while (cantidadPrestamo > limiteCredito);
-                do {
-                    String plazosTexto = JOptionPane.showInputDialog("Introduzca los años en los que desea devolver el préstamo" + "\n"
-                            + "Si los datos son incorrectos se pedirán de nuevo");
-                    plazos = Integer.parseInt(plazosTexto);
-                    if ((plazos > PLAZO_MAXIMO) || (plazos < PLAZO_MINIMO) || (edad + plazos > TOPE_DEVOLUCION)) {
-                        JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
-                                + "Años de devolución minimos: 5" + "\n"
-                                + "Años de devolución máximos: 30" + "\n"
-                                + "Atención: Los años en los que devuelva el préstamo sumados a su edad no podrá ser superior al límite permitido."
-                                + "\n" + "Límite permitido: 75 años.");
-                    }
-                } while ((plazos > PLAZO_MAXIMO) || (plazos < PLAZO_MINIMO) || (edad + plazos > TOPE_DEVOLUCION));
-                JOptionPane.showMessageDialog(null, "Su préstamo ha sido concedido");
-            } else {
-                JOptionPane.showMessageDialog(null, "El programa ha sido cancelado de manera exitosa");
-            }
-        } while (ejecucion == 1);
+
+        boolean seguir0 = false, seguir1 = false, seguir2 = false, seguir3 = false;
+
+
+        seguir1 = filtrarEdad(seguir0);
+        seguir2 = filtrarPrestamo(seguir0);
+        seguir3 = filtrarDevolucion(seguir0);
+
+        if ((seguir1==true && seguir2==true && seguir3==true)){
+            JOptionPane.showMessageDialog(null, "Solicitud Aprobada");
+        }
+        if (!(seguir1==true && seguir2==true && seguir3==true)){
+            JOptionPane.showMessageDialog(null, "Solicitud Denegada");
+        }
     }
 
 }
