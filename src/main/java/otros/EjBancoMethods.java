3212012a08c5c4947217203bb49ab05ad4aa4130
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-11 12:26:51 +0100

    Corrección para que pueda denegar solicitud, simplificación de código

--- src/main/java/otros/EjBancoMethods.java
+++ src/main/java/otros/EjBancoMethods.java
@@ -3,24 +3,28 @@ package otros;
 import javax.swing.JOptionPane;
 
 public class EjBancoMethods {
-
+    private int age;
+    
     private static boolean filtrarEdad(boolean a) {
         final int MIN_AGE = 18, MAX_AGE = 65;
-        int age = 0;
-        do {
-            String edadTexto = JOptionPane.showInputDialog("Introduzca su edad" + "\n"
-                    + "Si los datos son incorrectos se pedirán de nuevo");
+        int age;
+        
+            String edadTexto = JOptionPane.showInputDialog("Introduzca su edad");
             age = Integer.parseInt(edadTexto);
-            if (age < MIN_AGE || age > MAX_AGE) {
-                JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
-                        + "Rango de edad permitido: 18 - 65 años");
+            
+            if (age > MIN_AGE && age < MAX_AGE) {
+                return true;
             }
-        } while (age < MIN_AGE || age > MAX_AGE);
-
-        return (true);
+            else{
+                return false;
+            }
     }
 
+        
+    
+
     private static boolean filtrarPrestamo(boolean a) {
+        
         int salary = 0, moneyAsked = 0;
         final int MONTHS_IN_YEAR = 12;
         final double MULTIPLY_FACTOR = 5.85;
@@ -31,37 +35,34 @@ public class EjBancoMethods {
 
         creditLimit = (salary * MONTHS_IN_YEAR * MULTIPLY_FACTOR);
 
-        do {
-            String cantidadPrestamoTexto = JOptionPane.showInputDialog("Introduzca el dinero que desea solicitar" + "\n"
-                    + "Si los datos son incorrectos se pedirán de nuevo");
+        
+            String cantidadPrestamoTexto = JOptionPane.showInputDialog("Introduzca el dinero que desea solicitar");
             moneyAsked = Integer.parseInt(cantidadPrestamoTexto);
-            if (moneyAsked > creditLimit) {
-                JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
-                        + "El máximo solicitable será el salario anual multiplicado por el factor multiplicativo (5.85)");
+            
+            if (moneyAsked <= creditLimit) {
+                return true;
             }
-        } while (moneyAsked > creditLimit);
-        return (true);
+            else {
+                return false;
+            }
     }
 
     private static boolean filtrarDevolucion(boolean a) {
-        int deadlines = 0, age= 0;
+        
+        int deadlines = 0;
+        int age = 0;
         final int MIN_DEADLINES = 5, MAX_DEADLINES = 30, LIMIT_DEADLINES = 75;
 
-        do {
-            String edadTexto = JOptionPane.showInputDialog("Introduzca su edad de nuevo");
-            age = Integer.parseInt(edadTexto);
-            String plazosTexto = JOptionPane.showInputDialog("Introduzca los años en los que desea devolver el préstamo" + "\n"
-                    + "Si los datos son incorrectos se pedirán de nuevo");
+
+            String plazosTexto = JOptionPane.showInputDialog("Introduzca los años en los que desea devolver el préstamo");
             deadlines = Integer.parseInt(plazosTexto);
-            if ((deadlines > MAX_DEADLINES) || (deadlines < MIN_DEADLINES) || (age + deadlines > LIMIT_DEADLINES)) {
-                JOptionPane.showMessageDialog(null, "Los datos son incorrectos" + "\n"
-                        + "Años de devolución minimos: 5" + "\n"
-                        + "Años de devolución máximos: 30" + "\n"
-                        + "Atención: Los años en los que devuelva el préstamo sumados a su edad no podrá ser superior al límite permitido."
-                        + "\n" + "Límite permitido: 75 años.");
+            
+            if ((deadlines < MAX_DEADLINES) && (deadlines > MIN_DEADLINES) && (age + deadlines < LIMIT_DEADLINES)) {
+                return true;
             }
-        } while ((deadlines > MAX_DEADLINES) || (deadlines < MIN_DEADLINES) || (age + deadlines > LIMIT_DEADLINES));
-        return (true);
+            else {
+                return false;
+        }  
     }
 
     public static void main(String[] args) {
