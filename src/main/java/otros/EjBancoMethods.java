93f697edb3d4a573ce8cf98f12461a5b305d7c3b
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-11 10:34:36 +0100

    Establecidos requisitos del programa

--- src/main/java/otros/EjBancoMethods.java
+++ src/main/java/otros/EjBancoMethods.java
@@ -3,7 +3,17 @@ package otros;
 public class EjBancoMethods {
 
     public static void main(String[] args) {
-
+        /*
+        1-
+        Metodo que diga si la edad es válida o no.
+        Método reciba edad y devuelva resultado (Boolean).(Return)
+        2-
+        Método que diga si el dinero pedido es válido o no.
+        Método reciba dinero a pedir y devuelve resultado (Boolean). (Return)
+        3-
+        Método que diga si los años de devolución son correctos o no.
+        Método recibe años a devolver y devuelve resultado (Boolean). (Return)
+        */
     }
 
 }
