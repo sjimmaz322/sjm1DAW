2f3c1869dd51954528e941c218017f172c64653a
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-16 13:22:34 +0100

    Creado ejercicio

---
+++ src/main/java/ejerciciosclase/EjercicioClase05.java
@@ -0,0 +1,10 @@
+package ejerciciosclase;
+
+public class EjercicioClase05 {
+
+    public static void main(String[] args) {
+        //Ej 1-13 POO
+        //EJ 21, 22 y 23 (Apuntes Pág 8, clase Vehículo)
+    }
+
+}
