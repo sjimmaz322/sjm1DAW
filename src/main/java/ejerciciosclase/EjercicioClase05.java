fd5b3b0babd74de759b3ace3fedafea43ee7069f
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-16 14:01:34 +0100

    Añadidas preguntas a responder

--- src/main/java/ejerciciosclase/EjercicioClase05.java
+++ src/main/java/ejerciciosclase/EjercicioClase05.java
@@ -5,6 +5,38 @@ public class EjercicioClase05 {
     public static void main(String[] args) {
         //Ej 1-13 POO
         //EJ 21, 22 y 23 (Apuntes Pág 8, clase Vehículo)
+        /*
+        
+        EJERCICIOS DE INTRODUCCIÓN A LA P.O.O
+
+
+        ¿Qué es un programa orientado a objetos?
+
+        ¿Qué es una clase?
+
+        ¿Qué es un objeto?
+
+        ¿Cuáles son los pilares de la POO? Explica brevemente cada uno de ellos.
+
+        ¿Qué ventajas aporta la POO?
+
+        ¿Cuáles son las partes de una clase?
+
+        ¿Qué elementos componen la definición de una clase? Explica brevemente cada uno.
+
+        ¿Qué es un método constructor?
+
+        ¿Qué son los métodos de consulta o getters?
+
+        ¿Qué son los métodos modificadores o setters?
+
+        ¿Para qué se utiliza el puntero ‘this’?
+
+        ¿En qué consiste la instanciación de un objeto?
+
+        ¿Cuál es la función del método main()? ¿Cómo se declara el método main()?
+
+         */
     }
 
 }
