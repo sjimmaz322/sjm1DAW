14b76c259190eba6269382b22b77a5ad759ce81f
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-16 14:07:38 +0100

    Respuesta a las 6 primeras preguntas

--- src/main/java/ejerciciosclase/EjercicioClase05.java
+++ src/main/java/ejerciciosclase/EjercicioClase05.java
@@ -11,16 +11,24 @@ public class EjercicioClase05 {
 
 
         ¿Qué es un programa orientado a objetos?
-
+        Una colección de objetos que se crean, se relacionan entre si y de eliminan cuando dejan de ser necesarios
+        para diseñar aplicaciones informáticas.
+        
         ¿Qué es una clase?
-
+        Una representación abstracta de un conjunto de objetos que comparten los mismos atributos y comportamientos.
+        
         ¿Qué es un objeto?
-
+        Una instancia de una clase.
+        
         ¿Cuáles son los pilares de la POO? Explica brevemente cada uno de ellos.
-
+        La abstracción, mediante la creación de clases.
+        Encapsulamiento, permite ocultar la información de las clases ofreciendo al programador unas funciones.
+        
         ¿Qué ventajas aporta la POO?
-
+        La modularidad, la reutilización de códigos y la ocultación de la información.
+        
         ¿Cuáles son las partes de una clase?
+        La declaración y la definición.
 
         ¿Qué elementos componen la definición de una clase? Explica brevemente cada uno.
 
