12b0b13da63e0cb5be9d5df8617993bbfa7e5291
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-17 13:41:32 +0100

    Creada clase y comienzo de apuntaje

---
+++ src/main/java/otros/Apuntes.java
@@ -0,0 +1,12 @@
+package otros;
+
+public class Apuntes {
+
+    public static void main(String[] args) {
+        /*
+        Constructores se llaman IGUAL que la clase y no llevan nada más 
+        Public Nombre (Parámetros)
+        */
+    }
+
+}
