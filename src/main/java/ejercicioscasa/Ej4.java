3b2f8c0c59f9628e1d025c0ea5d8891c31e68763
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-17 14:11:09 +0100

    Creado método para obtener numeros aleatorio e inicialización con esos número de la variable num1 a num4

--- src/main/java/ejercicioscasa/Ej4.java
+++ src/main/java/ejercicioscasa/Ej4.java
@@ -1,5 +1,27 @@
 package ejercicioscasa;
 
-public class Ej4 {
+import java.util.Random;
 
+public class Ej4 {
+/*
+Crear contraseñas aleatorias que cumplan las siguientes restricciones:
+- 4 números entre 0 y 9 distintos
+- La suma de los dígitos 1 y 4 debe ser impar
+- La multiplicación de las digitos 2 y 3 debe ser par
+- Añadirá una letra aleatoria entre F y X (Mayúsculas).
+*/
+    public static void main(String[] args) {
+        int num1, num2, num3, num4;
+        char letra;
+        num1 = randomNumber();
+        num2 = randomNumber();
+        num3 = randomNumber();
+        num4 = randomNumber();
+        
+    }
+    private static int randomNumber(){
+        Random numRandom = new Random(10);
+        int numR = numRandom.nextInt();
+        return numR;
+    }
 }
