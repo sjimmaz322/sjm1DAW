ce3d5b512a9aa8ad91d4442643996b26681b574b
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 14:43:03 +0100

    Añadida clase y llamada a método

---
+++ src/main/java/pootema4/PruebaUtilidades.java
@@ -0,0 +1,15 @@
+package pootema4;
+
+import static pootema4.Utilidades.generarAleatorio;
+
+public class PruebaUtilidades {
+
+    public static void main(String[] args) {
+        
+        int num, num2;
+        num = generarAleatorio();
+        System.out.println(num);
+        
+    }
+
+}
