762f1028c9877e83643ef558740463f6622f3d35
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 14:37:45 +0100

    Añadido método leerEnteros

--- src/main/java/pootema4/Utilidades.java
+++ src/main/java/pootema4/Utilidades.java
@@ -1,17 +1,23 @@
 package pootema4;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Utilidades {
 
     /*
     Quiero una clase llamada Utilidades, sin atributos, con dos métodos: generarAleatorios
-    en un rango dado y leer Enteros con Scanner. Luego probar en un main ambos métodos creando un objeto de la clase Utilidades
+    en un rango dado y leerEnteros con Scanner. Luego probar en un main ambos métodos creando un objeto de la clase Utilidades
      */
     public static int generarAleatorio(int numMax, int nunMin) {
 
         Random numAleatorio = new Random();
-        int numRandom = numAleatorio.nextInt();
+        int numRandom = numAleatorio.nextInt(100)+1;
         return numRandom;
+    }
+    public static int leeEnteros(){
+        Scanner sc = new Scanner(System.in);
+        int num =sc.nextInt();
+        return num;
     }
 }
