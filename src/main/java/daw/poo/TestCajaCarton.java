000e3b3949b1c352ec409408e5c6b4fbe3cbabfc
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 14:29:57 +0100

    Añadidos get para recibir las medidas

--- src/main/java/daw/poo/TestCajaCarton.java
+++ src/main/java/daw/poo/TestCajaCarton.java
@@ -3,11 +3,18 @@ package daw.poo;
 public class TestCajaCarton {
 
     public static void main(String[] args) {
+
+        CajaCarton cajaGrande = new CajaCarton(250, 400, 500, 200);
+
+        CajaCarton cajaChica = new CajaCarton(25, 30, 15, 17);
         
-        CajaCarton cajaGrande = new CajaCarton(500, 300, 300, 50);
-        
-        CajaCarton cajaChica = new CajaCarton(240, 170, 87, 20);
-        
+        System.out.println("Las características de la caja grande son:");
+        System.out.println(cajaGrande.getAncho() + " mm " + cajaGrande.getAlto() + " mm " +
+                cajaGrande.getlargo() + " mm " + cajaGrande.getPeso() + " gr");
+        System.out.println(" ");
+        System.out.println("Las características de la caja chica son:");
+        System.out.println(cajaChica.getAncho() + " mm " + cajaChica.getAlto() +
+                " mm " + cajaChica.getlargo() + " mm " + cajaChica.getPeso() + " gr");
     }
 
 }
