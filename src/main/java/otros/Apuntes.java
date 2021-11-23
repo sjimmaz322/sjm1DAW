c2e3bc3f4244174f4e50bfb5f357282dbc13a5a3
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 13:19:11 +0100

    Añadido opciones de try catch

--- src/main/java/otros/Apuntes.java
+++ src/main/java/otros/Apuntes.java
@@ -1,5 +1,8 @@
 package otros;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Apuntes {
 
     public static void main(String[] args) {
@@ -7,6 +10,14 @@ public class Apuntes {
         Constructores se llaman IGUAL que la clase y no llevan nada más 
         Public Nombre (Parámetros)
         */
+        int numero;
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Dame un número");
+            numero = sc.nextInt();
+        }catch (InputMismatchException ime){
+            System.out.println("No es un número");
+        }
     }
 
 }
