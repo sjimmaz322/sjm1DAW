03204153e5ba653ffc7fc5b5e1ab5797d1732c2f
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 14:43:17 +0100

    Añadidas cosas (Mirar luego)

--- src/main/java/pootema4/Utilidades.java
+++ src/main/java/pootema4/Utilidades.java
@@ -4,20 +4,22 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Utilidades {
-
+    
     /*
     Quiero una clase llamada Utilidades, sin atributos, con dos métodos: generarAleatorios
     en un rango dado y leerEnteros con Scanner. Luego probar en un main ambos métodos creando un objeto de la clase Utilidades
      */
-    public static int generarAleatorio(int numMax, int nunMin) {
-
+    public static int generarAleatorio() {
         Random numAleatorio = new Random();
-        int numRandom = numAleatorio.nextInt(100)+1;
+        int numRandom = numAleatorio.nextInt();
         return numRandom;
     }
-    public static int leeEnteros(){
+
+    public static int leeEnteros() {
         Scanner sc = new Scanner(System.in);
-        int num =sc.nextInt();
+        int num = sc.nextInt();
         return num;
     }
+    
+    
 }
