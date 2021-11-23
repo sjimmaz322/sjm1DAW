93cbb785904435e694b6c474cf061a5057201894
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 14:12:31 +0100

    Creada clase, constructor, getter y setter

---
+++ src/main/java/daw/poo/CajaCarton.java
@@ -0,0 +1,42 @@
+package daw.poo;
+
+public class CajaCarton {
+
+    private double ancho;
+    private double alto;
+    private double largo;
+    private double peso;
+
+    public double getAncho() {
+        return ancho;
+    }
+
+    public double getAlto() {
+        return alto;
+    }
+
+    public double getlargo() {
+        return largo;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double setAncho() {
+        return this.ancho;
+    }
+
+    public double setAlto() {
+        return this.alto;
+    }
+
+    public double setLargo() {
+        return this.largo;
+    }
+
+    public double setPeso() {
+        return this.peso;
+    }
+
+}
