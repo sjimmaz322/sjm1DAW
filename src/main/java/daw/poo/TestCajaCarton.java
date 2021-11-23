9464d84a4ca415577af14d3596f7079214fe9c98
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 14:23:38 +0100

    Creada clase y añadidos dos objetos cajas

---
+++ src/main/java/daw/poo/TestCajaCarton.java
@@ -0,0 +1,13 @@
+package daw.poo;
+
+public class TestCajaCarton {
+
+    public static void main(String[] args) {
+        
+        CajaCarton cajaGrande = new CajaCarton(500, 300, 300, 50);
+        
+        CajaCarton cajaChica = new CajaCarton(240, 170, 87, 20);
+        
+    }
+
+}
