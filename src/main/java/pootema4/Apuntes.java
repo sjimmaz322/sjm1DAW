12dc3dfbed06eb1dc117cbb87b081094d74f166c
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-23 14:12:42 +0100

    Cambio de clase

--- src/main/java/pootema4/Apuntes.java
+++ src/main/java/pootema4/Apuntes.java
@@ -5,6 +5,13 @@ public class Apuntes {
     public static void main(String[] args) {
     /*
         Encapsulamiento de objetos siempre
+        Si hay condición en el constructor debe haber condición en setter
+        this se "trasforma" en la variable del objeto sobre el que estamos trabajando 
+        
+        Ej 17, 18, 19, 20, 24 y 25
+        
+        Quiero una clase llamada utilidades sin atributos, con dos métodos generarAleatorios en un rango dado
+        y leer enteros con Scanner. Luego probar en un main ambos métodos creando un objeto de la clase Utilidades
         */
     
     }
