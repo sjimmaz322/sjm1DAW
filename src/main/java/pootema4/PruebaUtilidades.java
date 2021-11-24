d38705d4b980c874d921f99f723c40d057d83556
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-24 14:24:35 +0100

    Mejora de código y cambio de nombre método leer

--- src/main/java/pootema4/PruebaUtilidades.java
+++ src/main/java/pootema4/PruebaUtilidades.java
@@ -1,15 +1,20 @@
 package pootema4;
 
 import static pootema4.Utilidades.generarAleatorio;
+import static pootema4.Utilidades.leerEnteros;
 
 public class PruebaUtilidades {
 
     public static void main(String[] args) {
+       
         
         int num, num2;
         num = generarAleatorio();
         System.out.println(num);
         
+        System.out.println("Introduce un número");
+        num2 = leerEnteros();
+        System.out.println(num2);
     }
 
 }
