f51b457c9d0dd7ad271192c0fd0dd207ed639201
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-25 11:58:05 +0100

    Setted empleado1

--- src/main/java/pootema4/Prueba.java
+++ src/main/java/pootema4/Prueba.java
@@ -12,6 +12,19 @@ public class Prueba {
         
         System.out.println("Sobre song1: " + song1.getAutor() + " es el autor y " + song1.getTitulo()+ " es su obra");
         System.out.println(song2.toString());
+        
+        
+     //   nombre, apellidos, NIF, sueldoBase, pagoHoraExtra, horasExtrasRealizadas, IRPF, estadoCivil, numHijos
+        Empleado empleado1 = new Empleado();
+        empleado1.setNombre("Samuel Alejandro");
+        empleado1.setApellidos("Jiménez Mazas");
+        empleado1.setNIF("31014322-H");
+        empleado1.setSueldoBase("1800");
+        empleado1.setPagoHoraExtra("17");
+        empleado1.setHorasExtrasRealizadas("20");
+        empleado1.setIRPF("17");
+        empleado1.setNumHijos("0");
+        
     }
 
 }
