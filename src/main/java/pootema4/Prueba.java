ed90295196bb9af0415dd67ff5e9bdd1cb336773
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-11-25 12:36:54 +0100

    Cambios menores

--- src/main/java/pootema4/Prueba.java
+++ src/main/java/pootema4/Prueba.java
@@ -4,6 +4,8 @@ public class Prueba {
 
     public static void main(String[] args) {
         
+        
+        
         Cancion song1 = new Cancion();
         Cancion song2 = new Cancion("Este es el nombre", "Este la canta");
         
@@ -22,19 +24,20 @@ public class Prueba {
         empleado1.setNombre("Samuel Alejandro");
         empleado1.setApellidos("Jiménez Mazas");
         empleado1.setNIF("31014322-H");
-        empleado1.setSueldoBase("1800");
-        empleado1.setPagoHoraExtra("17");
-        empleado1.setHorasExtrasRealizadas("20");
-        empleado1.setIRPF("17");
-        empleado1.setEstadoCivil("Soltero");
-        empleado1.setNumHijos("0");
+        empleado1.setSueldoBase(1800);
+        empleado1.setPagoHoraExtra(17);
+        empleado1.setHorasExtrasRealizadas(20);
+        empleado1.setIRPF(17);
+        empleado1.setEstadoCivil(false);
+        empleado1.setNumHijos(0);
         
-        Empleado empleado2 = new Empleado("Humano","Humanez","12345678-Z","500","17","0","15","Casado","2");
+        Empleado empleado2 = new Empleado("Humano","Humanez","12345678-Z",500,17,0,15,true,2);
         
         System.out.println("Los datos del empleado 1 son: Nombre - "+empleado1.getNombre()+" Apellidos - "+empleado1.getApellidos()+
                 " NIF - "+empleado1.getNIF()+"\n Sueldo base - "+empleado1.getSueldoBase()+" Pago por hora extra - "+
                 empleado1.getPagoHoraExtra()+" Horas extras realizadas - "+empleado1.getHorasExtrasRealizadas()+"\n Paga un IRPF de "+
                 empleado1.getIRPF()+" El empleado es - "+empleado1.getEstadoCivil()+" y su número de hijos es de "+empleado1.getNumHijos());
+       // System.out.println("El empleado 1 cobrará por su horas extras un plud de :" + );
         System.out.println(" ");
         System.out.println(empleado2.toString());
     }
