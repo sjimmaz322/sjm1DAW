382c2e4995a2aa3c61d41e63e82aed4259e27002
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-01 14:21:25 +0100

    Añadida cafetera 3

--- src/main/java/pootema4/Prueba2.java
+++ src/main/java/pootema4/Prueba2.java
@@ -6,8 +6,11 @@ public class Prueba2 {
         
         Cafetera_VICO c1 = new Cafetera_VICO(8);
         Cafetera_VICO c2 = new Cafetera_VICO();
+        Cafetera_VICO c3 = new Cafetera_VICO(100,200);
+        
         System.out.println(c1);
         System.out.println(c2);
+        System.out.println(c3);
         
     }
 
