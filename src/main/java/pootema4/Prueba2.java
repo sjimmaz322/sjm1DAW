1fb40f1c982a90be349553deab27ad78aecfd357
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-01 13:33:45 +0100

    Creada clase de pruebas 2

---
+++ src/main/java/pootema4/Prueba2.java
@@ -0,0 +1,12 @@
+package pootema4;
+
+public class Prueba2 {
+
+    public static void main(String[] args) {
+        Cafetera cafetera1 = new Cafetera();
+        
+        System.out.println(cafetera1.toString());
+        
+    }
+
+}
