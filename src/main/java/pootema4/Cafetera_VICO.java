f7510d524bfe5e328663db9b6195a99e54f2c468
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-01 14:12:23 +0100

    Bla

--- src/main/java/pootema4/Cafetera_VICO.java
+++ src/main/java/pootema4/Cafetera_VICO.java
@@ -1,6 +1,58 @@
 package pootema4;
-
+//partes de una clase
+//Definición de la clase
 public class Cafetera_VICO {
-
     
+    //Atributos encapsulados, campos o propiedades.
+    private double capacidadMaxima;
+    private double cantidadActual;
+    //private double cantidadAServir;
+    //private double cantidadAgregar;
+
+    //Métodos
+    //Especial mención contructores, getters y setters.
+    //Constructor por defecto sin parámetros
+
+    public Cafetera_VICO() {
+        this.capacidadMaxima = 1000;
+        this.cantidadActual = 0;
+    }
+    //Si la capacidad máxima es negativa pasa a positiva
+    public Cafetera_VICO(double capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+        if (capacidadMaxima < 10){     
+            this.capacidadMaxima = 10; //Para no poder crear una cafetara de capacidad 0
+        }                              
+        this.cantidadActual=this.capacidadMaxima;
+    }
+
+    public Cafetera_VICO(double capacidadMaxima, double cantidadActual) {
+        this.capacidadMaxima = capacidadMaxima;
+        
+        if (capacidadMaxima < 10){     
+            this.capacidadMaxima = 10; //Para no poder crear una cafetara de capacidad 0
+            
+        if (cantidadActual > this.capacidadMaxima){
+            this.cantidadActual = this.capacidadMaxima;
+            }
+        }                              
+        
+        
+        this.cantidadActual=this.capacidadMaxima;
+    }
+    
+    
+    
+    
+    //Implicación, utilidad public y private en los atributos y métodos de una clase --> Buscar que lo pide
+    
+    //Encapsulamiento
+    
+    //toString
+    @Override
+    public String toString() {
+        return "Cafetera_VICO{" + "capacidadMaxima=" + capacidadMaxima + ", cantidadActual=" + cantidadActual + '}';
+    }
+
+   
 }
