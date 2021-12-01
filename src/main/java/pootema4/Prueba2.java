1a7ae79215558392b788fa812441c78c6fcc24ff
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-01 14:12:31 +0100

    Blabla

--- src/main/java/pootema4/Prueba2.java
+++ src/main/java/pootema4/Prueba2.java
@@ -3,9 +3,11 @@ package pootema4;
 public class Prueba2 {
 
     public static void main(String[] args) {
-        Cafetera cafetera1 = new Cafetera();
         
-        System.out.println(cafetera1.toString());
+        Cafetera_VICO c1 = new Cafetera_VICO(8);
+        Cafetera_VICO c2 = new Cafetera_VICO();
+        System.out.println(c1);
+        System.out.println(c2);
         
     }
 
