9b6d2dc9a945230e3d477df526d4536048daa297
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-01 13:33:33 +0100

    Que dios me ampare

--- src/main/java/pootema4/Cafetera.java
+++ src/main/java/pootema4/Cafetera.java
@@ -2,40 +2,22 @@ package pootema4;
 
 public class Cafetera {
     
-   private double capacidadMaxima, cantidadActual, cantidadAServir;
-   
-   
+    //Interfaz son los métodos públicos.
     
-    
-//Desarrolla una clase Cafetera con atributos capacidadMaxima (la cantidad máxima de café que puede contener la cafetera)
-//y cantidadActual (la cantidad actual de café que hay dentro de la cafetera). Implementa, al menos, los siguientes métodos:
-//Constructor predeterminado, sin parámetros: establece la capacidad máxima en 1000 (c.c.) y la cantidad actual en cero (cafetera vacía).
-//
-//Constructor, que recibe sólo la capacidad máxima de la cafetera; inicializa la cantidad actual de café igual a la capacidad máxima
-//(cafetera llena).
-//
-//Constructor, que recibe la capacidad máxima y la cantidad actual. Si la cantidad actual que se pasa es mayor que la capacidad máxima
-//de la cafetera, se ajustará la cantidad actual a la capacidad máxima.
-//
-//llenarCafetera(): hace que la cantidad actual sea igual a la capacidad.
-//
-//servirTaza(double cantidadAServir): simula la acción de servir una taza con la capacidad indicada. Si la cantidad actual de café “
-//no alcanza” para llenar la taza, se sirve lo que quede.
-//
-//vaciarCafetera(): pone la cantidad de café actual en cero.
+   private double capacidadMaxima, cantidadActual, cantidadAServir, cantidadAgregar;
 
     public Cafetera() {
         this.capacidadMaxima = 1000;
         this.cantidadActual = 0;
         this.cantidadAServir = 0;
         if (this.cantidadAServir < this.cantidadActual){this.cantidadAServir=this.cantidadActual;}
+        this.cantidadAgregar = 25;
     }
 
     public Cafetera(double capacidadMaxima) {this.cantidadActual = this.capacidadMaxima;} 
     
     public Cafetera(double capacidadMaxima, double cantidadActual) {
-        this.capacidadMaxima = capacidadMaxima;
-        if (capacidadMaxima>1000){
+        if (cantidadActual>capacidadMaxima){
             this.capacidadMaxima = 1000;
         }
         this.cantidadActual = cantidadActual;
@@ -67,15 +49,39 @@ public class Cafetera {
         this.cantidadActual=this.capacidadMaxima;
         return this.cantidadActual;
     }
+    
     public double vaciarCafetera(){
-        this.cantidadActual = ;
-    }return cantidadActual;
-//    public double servirTaza(){
-//        this.cantidadAServir = cantidadAServir;
-//        if (this.cantidadAServir < this.cantidadActual){
-//            this.cantidadAServir=this.cantidadActual;
-//        }
-//    }return this.cantidadAServir;
+        this.cantidadActual = 0;
+        return this.cantidadActual;
+    }
+    
+    public double servirTaza(){
+        if (this.cantidadAServir < this.cantidadActual){
+            this.cantidadAServir=this.cantidadActual;
+        }
+    return this.cantidadAServir;
+    }
+    public double agregarCafe(double cantidadAgregar){
+        cantidadActual = cantidadActual + cantidadAgregar;
+        if ((cantidadActual + cantidadAgregar)>capacidadMaxima){
+            return capacidadMaxima;
+        }
+        return cantidadActual;
+    }
+
+    public double getCantidadAgregar() {
+        return cantidadAgregar;
+    }
+
+    public void setCantidadAgregar(double cantidadAgregar) {
+        this.cantidadAgregar = cantidadAgregar;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "Cafetera{" + "capacidadMaxima=" + capacidadMaxima + ", cantidadActual=" + cantidadActual + ", cantidadAServir=" + cantidadAServir + ", cantidadAgregar=" + cantidadAgregar + '}';
+    }
 
     
 
