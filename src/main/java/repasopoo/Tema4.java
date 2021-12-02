b1b06e43ddba1a801c46430107c1b78198dbae62
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-02 10:59:25 +0100

    Más apuntes

--- src/main/java/repasopoo/Tema4.java
+++ src/main/java/repasopoo/Tema4.java
@@ -7,5 +7,15 @@ Identidad - Como se llama
 Estado - Valores concretos que tienen sus atributos
 Comportamiento - Común para todos los objetos, son los métodos
     Retricción en constructor debe ir también en método set
+    
+    Métodos Static o de clase:
+    - No requiere creación de objeto
+    - Usamos el nombre de la clase y un punto
+    - Ej: Math.algo();
+    
+    Métods no Static o de instancia:
+    - Si requiere crear objeto
+    - Ej: Scanner
+    - Siempre con el nombre del objeto por delante
 */
 }
