c037f506c03240e1076e442d24887d849d977767
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-02 10:52:37 +0100

    Puntualización hecha

--- src/main/java/repasopoo/Tema4.java
+++ src/main/java/repasopoo/Tema4.java
@@ -5,7 +5,7 @@ public class Tema4 {
     Características de los objetos:
 Identidad - Como se llama
 Estado - Valores concretos que tienen sus atributos
-Comportamiento - Común para todos los objetos
+Comportamiento - Común para todos los objetos, son los métodos
     Retricción en constructor debe ir también en método set
 */
 }
