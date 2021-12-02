dbad4da03c87a2dbd255b34df9ba491160c54570
Sjimmaz322 <sjimmaz322@g.educaand.es>
2021-12-02 12:13:40 +0100

    Creada clase main y probado el RandomStringUtils

---
+++ src/main/java/PruebaCuentaBancaria.java
@@ -0,0 +1,13 @@
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+
+public class PruebaCuentaBancaria {
+
+    
+    public static void main(String[] args) {
+        
+        System.out.println(RandomStringUtils.randomNumeric(15).toUpperCase());
+    }
+    
+}
