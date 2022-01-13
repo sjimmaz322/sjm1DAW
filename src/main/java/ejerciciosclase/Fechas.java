71f3480ad4511411dad9433da978c828bfd1b912
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-13 12:26:21 +0100

    Añadidos constructores

--- src/main/java/ejerciciosclase/Fechas.java
+++ src/main/java/ejerciciosclase/Fechas.java
@@ -2,4 +2,21 @@ package ejerciciosclase;
 
 public class Fechas {
     
+    private int dia,mes,anyo;
+
+    public Fechas() {
+        this.dia = 1;
+        this.mes = 1;
+        this.anyo = 2022;
+    }
+
+    public Fechas(int dia, int mes, int anyo) {
+        this.dia = dia;
+        this.mes = mes;
+        this.anyo = anyo;
+    }
+    
+    
+    
+    
 }
