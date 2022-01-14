4bc05aa92043df99ebe66c7c135ffdd07e0c36de
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-14 12:54:34 +0100

    Cambios en objetos a modalidad 1

--- src/main/java/mecanica/Transporte.java
+++ src/main/java/mecanica/Transporte.java
@@ -8,8 +8,8 @@ public class Transporte {
         Ventana v1 = new Ventana(true, true);
         Ventana v2 = new Ventana(true, true);
         
-        Puerta pp = new Puerta(false, new Ventana(true,true));
-        Puerta pc = new Puerta(false, new Ventana(true,true));
+        Puerta pp = new Puerta(false,v1);
+        Puerta pc = new Puerta(false,v2);
         
         Rueda rdi = new Rueda(50,true);
         Rueda rdd = new Rueda(50,true);
@@ -18,7 +18,7 @@ public class Transporte {
         
         Coche auto = new Coche(m,pp,pc,rdi,rdd,rti,rtd);
         
-        Coche auto2 = new Coche(new Motor(2.0, false),new Puerta(false,v1),new Puerta(false,v2),new Rueda(50,true),new Rueda(50,true),new Rueda(50,true),new Rueda(50,true));
+        Coche auto2 = new Coche(new Motor(2.0, false),new Puerta(false, new Ventana (true,true)),new Puerta(false, new Ventana(true,true)),new Rueda(50,true),new Rueda(50,true),new Rueda(50,true),new Rueda(50,true));
         
         System.out.println(auto);
         
@@ -27,6 +27,8 @@ public class Transporte {
         System.out.println(auto2);
         
         auto2.getPuertaPiloto().getVentana().setTintado(false);
+        auto2.getDelanteraIzda().getDiametro();
+        auto2.getMotor().arrancar();
         
     }
 
