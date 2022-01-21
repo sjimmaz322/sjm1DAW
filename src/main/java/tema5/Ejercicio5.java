25503c7974ef25d53d3b05fdde77fc863fe5ff1e
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-21 14:31:43 +0100

    Añadidos métodos mediaPositivos y mediaNegativos.

--- src/main/java/tema5/Ejercicio5.java
+++ src/main/java/tema5/Ejercicio5.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tema5;
 
 import java.util.Scanner;
 
-/**
- *
- * @author samjimmaz
- */
 public class Ejercicio5 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
 
         int[] numbers = new int[10];
@@ -58,5 +47,36 @@ public class Ejercicio5 {
             }
         }
         return contador;
+    }
+
+    private void mostrarArray(int[] numbers) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("El valor de la posición " + i + " del Array es " + numbers[i]);
+        }
+    }
+
+    private int mediaPositivos(int[] numbers) {
+        int media = 0;
+        int suma = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > 0) {
+                suma += numbers[i];
+            }
+        }
+        media = suma / numbers.length;
+        return media;
+    }
+
+    private int mediaNegativos(int[] numbers) {
+        int media = 0;
+        int suma = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < 0) {
+                suma -= numbers[i];
+            }
+        }
+        media = suma / numbers.length;
+        return media;
     }
 }
