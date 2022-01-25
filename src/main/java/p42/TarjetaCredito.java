89e0acf3a8fdb3f166ccae9e1a1966c611e1e1a3
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-25 13:45:15 +0100

    Añadidos parámetros, constructores, getters y setters

--- src/main/java/p42/TarjetaCredito.java
+++ src/main/java/p42/TarjetaCredito.java
@@ -1,13 +1,67 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package p42;
 
-/**
- *
- * @author samjimmaz
- */
+import java.time.YearMonth;
+
 public class TarjetaCredito {
+
+    private final String ENTIDAD_EMISORA, NUM_TARJETA, TITULAR;
+    private double saldo;
+    private boolean estado = false; //false para ddesactivada y true para activada.
+    private YearMonth CADUCIDAD;
+    public static final double MIN = 500;
+    public static final double MAX = 3000;
+    private String pin;
+
+    public TarjetaCredito() {
+        this.ENTIDAD_EMISORA = "Tetimo";
+        this.NUM_TARJETA = "1234-5678-8765-4321";
+        this.TITULAR = "Nombre Apellido";
+        this.saldo = 1000;
+        this.CADUCIDAD = YearMonth.now().plusYears(3);
+        this.pin = "1234";
+    }
+
+    public TarjetaCredito(String ENTIDAD_EMISORA, String NUM_TARJETA, String TITULAR, double saldo, YearMonth CADUCIDAD, String pin) {
+        this.ENTIDAD_EMISORA = ENTIDAD_EMISORA;
+        this.NUM_TARJETA = NUM_TARJETA;
+        this.TITULAR = TITULAR;
+        this.saldo = saldo;
+        this.CADUCIDAD = CADUCIDAD;
+        this.pin = pin;
+    }
+
+    public String getENTIDAD_EMISORA() {
+        return ENTIDAD_EMISORA;
+    }
+
+    public String getNUM_TARJETA() {
+        return NUM_TARJETA;
+    }
+
+    public String getTITULAR() {
+        return TITULAR;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public YearMonth getCADUCIDAD() {
+        return CADUCIDAD;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
     
 }
+
