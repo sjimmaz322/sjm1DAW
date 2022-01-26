48d3a88811c78f2c0ff033f6d78e6e3541ef5c7e
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-26 14:09:02 +0100

    Cambios menores

--- src/main/java/p42/PruebaTarjeta.java
+++ src/main/java/p42/PruebaTarjeta.java
@@ -1,9 +1,14 @@
 package p42;
 
+import java.time.YearMonth;
+
 public class PruebaTarjeta {
 
     public static void main(String[] args) {
         
+        TarjetaCredito tarjeta1 = new TarjetaCredito("Tetimo", "1234567891234567", "Nombre Apellido", 1500, YearMonth.of(2022, 31), "1234");
+ 
+        System.out.println(tarjeta1.toString());
     }
 
 }
