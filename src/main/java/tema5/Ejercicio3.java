05deafffa5e00ce2af8c8e15a6496d45425ec751
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-27 10:45:46 +0100

    Terminado ejercicio

--- src/main/java/tema5/Ejercicio3.java
+++ src/main/java/tema5/Ejercicio3.java
@@ -1,9 +1,44 @@
 package tema5;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ejercicio3 {
 
     public static void main(String[] args) {
-
+        
+        double[] arr1 = new double[20];
+        double[] arr2;
+        boolean iguales;
+        
+        //Rellenado del arr1
+        Arrays.fill(arr1, 7.5);
+        
+        //Copia de arr1 en arr2
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        
+        //Mostramos por consola ambos String
+        System.out.println("Primer array: "+Arrays.toString(arr1));
+        System.out.println("Segundo array: "+Arrays.toString(arr2));
+        
+        //Comparación
+        iguales = Arrays.equals(arr1, arr2);
+       
+        //Muestra de resultado con operador ternario
+        System.out.println(Arrays.equals(arr1, arr2)?"Son iguales":"No son iguales");
+        //Creamos objeto random
+        Random numRandom = new Random();
+        //Creamos el número random
+        int indice = numRandom.nextInt(arr1.length);
+        //Cambiamos un lugar aleatorio de arr2
+        arr2[indice] = 6.3;
+        
+        //Mostramos por consola ambos String
+        System.out.println("Primer array: "+Arrays.toString(arr1));
+        System.out.println("Segundo array: "+Arrays.toString(arr2));
+        
+        //Muestra de resultado con operador ternario
+        System.out.println(Arrays.equals(arr1, arr2)?"Son iguales":"No son iguales");
     }
     
 }
