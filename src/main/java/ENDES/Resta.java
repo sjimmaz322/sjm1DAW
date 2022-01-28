d0f330ec9ba80e1ceda12c5c6dc29804d9ef3fd4
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-28 10:58:51 +0100

    Creada clase y copiados los métodos

---
+++ src/main/java/ENDES/Resta.java
@@ -0,0 +1,13 @@
+package ENDES;
+
+public class Resta {
+    public double getResta(double a, double b)
+ {
+ return a - b;
+ }
+
+ public double decrementa(double a)
+ {
+ return a - 1;
+ }
+}
---
+++ src/main/java/ENDES/Suma.java
@@ -0,0 +1,12 @@
+package ENDES;
+
+public class Suma {
+    public double getSuma(double a, double b) {
+        return a + b;
+    }
+
+    public double incrementa(double a) {
+        return a + 1;
+    }
+}
+
