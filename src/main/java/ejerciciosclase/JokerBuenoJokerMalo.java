0a735f348e8cac0fdf256086d5e06103438b4854
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-01-31 12:51:14 +0100

    Cambio nombre variables para mejor descipción

--- src/main/java/ejerciciosclase/JokerBuenoJokerMalo.java
+++ src/main/java/ejerciciosclase/JokerBuenoJokerMalo.java
@@ -6,18 +6,18 @@ import tema5.MetodosArrays;
 public class JokerBuenoJokerMalo {
 
     public static void main(String[] args) {
-        int[] joker = MetodosArrays.crearArrayFijo(7);
-        MetodosArrays.rellenarArrayRandom(joker, 10);
-        int[] jugador = MetodosArrays.crearArrayFijo(7);
-        MetodosArrays.rellenarArray(jugador);
+        int[] arrayJoker = MetodosArrays.crearArrayFijo(7);
+        MetodosArrays.rellenarArrayRandom(arrayJoker, 10);
+        int[] arrayJugador = MetodosArrays.crearArrayFijo(7);
+        MetodosArrays.rellenarArray(arrayJugador);
         System.out.println("-");
-        MetodosArrays.compararArrays(joker, jugador);
-        if (MetodosArrays.compararArrays(joker, jugador) == true) {
+        MetodosArrays.compararArrays(arrayJoker, arrayJugador);
+        if (MetodosArrays.compararArrays(arrayJoker, arrayJugador) == true) {
             System.out.println("ENHORABUENA \nUSTED HA GANADO EL PREMIO GORDO");
         } else {
-            Arrays.sort(jugador);
-            Arrays.sort(joker);
-            if (MetodosArrays.compararArrays(joker, jugador) == true) {
+            Arrays.sort(arrayJugador);
+            Arrays.sort(arrayJoker);
+            if (MetodosArrays.compararArrays(arrayJoker, arrayJugador) == true) {
                 System.out.println("ENHORABUENA \nSU COMBINACIÓN NUMÉRICA TIENE PREMIO");
             } else {
                 System.out.println("LO SENTIMOS, MÁS SUERTE LA PRÓXIMA");
