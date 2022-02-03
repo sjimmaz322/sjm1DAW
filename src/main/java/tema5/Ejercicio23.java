c4cba60af67b93270ff1a475cf47a20f2dbe3080
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-02-03 20:34:51 +0100

    Realizada primera parte del ejercicio

--- src/main/java/tema5/Ejercicio23.java
+++ src/main/java/tema5/Ejercicio23.java
@@ -1,9 +1,23 @@
 package tema5;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Ejercicio23 {
 
     public static void main(String[] args) {
-
+        ArrayList<Integer> lista1 = new ArrayList<>();
+        
+        Random rd = new Random();
+        int tamanio = rd.nextInt(15-5+1)+5;
+        
+        for (int i = 0; i < tamanio; i++) {
+            Integer aux = rd.nextInt(100-50+1)+50 ;
+            lista1.add(aux);
+               
     }
-    
+    for (Integer tmp : lista1) {//forEach para ver todos los sitios
+            System.out.print(tmp + " - ");
+        }
+}
 }
