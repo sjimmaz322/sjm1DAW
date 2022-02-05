8a06355e74e6e48ab97ce433056d87f19e31ddb0
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-02-05 10:45:20 +0100

    Añadida solución

--- src/main/java/tema5/Ejercicio23.java
+++ src/main/java/tema5/Ejercicio23.java
@@ -5,6 +5,18 @@ import java.util.Collections;
 import java.util.Random;
 import javax.swing.SpringLayout;
 
+/*
+Se desea manejar dos listas (ArrayList) de Integer. La cantidad de elementos de la lista 1 debe ser un número aleatorio entre 5 y 15.
+La cantidad de elementos de la lista 2 debe ser un número aleatorio entre 10 y 20.
+El rango de los números que serán agregados a la lista DEBE estar entre 50 y 100.
+Se pide construir una aplicación en Java que permita cumplir con la siguiente funcionalidad:
+Imprimir las listas que han sido generadas. Usa for-each.
+Crear una tercera lista que contenga los elementos que están en la lista 1 y NO ESTÁN en la lista 2 
+Imprimir la tercera lista. 
+Crear una cuarta lista que contenga los elementos de la lista 1 que son pares y los elementos de la lista 2 que son impares 
+Imprimir la cuarta lista. Usa una expresión lambda.
+
+ */
 public class Ejercicio23 {
 
     public static void rellenarArrayListRandom(int tamanio, ArrayList<Integer> aux) {
