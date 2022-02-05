3485f0f13ab24e93e92690be89106a109d9d2d85
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-02-05 18:51:26 +0100

    Pruebas de enum tarjetas

--- src/main/java/enumejemplos/TestEnum.java
+++ src/main/java/enumejemplos/TestEnum.java
@@ -4,6 +4,9 @@
  */
 package enumejemplos;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+
 /**
  *
  * @author samjimmaz
@@ -13,7 +16,11 @@ public class TestEnum {
     public static void main(String[] args) {
         
             Pizza bbq = Pizza.BARBACOA;
+            Tarjetas card1 = Tarjetas.TARJETA1;
+            Tarjetas card3 = Tarjetas.TARJETA3;
            
+            
+            
             Pizza.values();//Devuelve array con todos los objetos.
             
             switch(bbq){
@@ -22,5 +29,14 @@ public class TestEnum {
                 case CARBONARA:
                     break;
             }
+            if (card1.getCaducidad().isAfter(YearMonth.now())){
+                System.out.println("La cosa va bien");
+            }
+            if (card3.getCaducidad().isAfter(YearMonth.now())){
+                System.out.println("La cosa va bien");
+            }else{
+                System.out.println("Tarjeta caducada");
+            }
+            
     }
 }
