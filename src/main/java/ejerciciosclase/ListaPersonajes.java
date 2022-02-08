64831d574c302ec8acf0f227a05751ae46dee771
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-02-08 14:38:51 +0100

    Añadidos metodos buscar el primero que cumpla un parámetro y buscar todos los que cumplan los parámetros

--- src/main/java/ejerciciosclase/ListaPersonajes.java
+++ src/main/java/ejerciciosclase/ListaPersonajes.java
@@ -22,7 +22,58 @@ public class ListaPersonajes {
         System.out.println("Imprimiendo lista...");
         listaPersonajes.forEach(System.out::println);
     }
-   
     
+    public void insertarPersonaje(Personaje personaje){
+        
+        if (personaje != null) {
+            listaPersonajes.add(personaje);
+        }
+    }
+    
+    public boolean eliminarPersonaje(Personaje personaje){
+
+        return listaPersonajes.remove(personaje);
+    }
+    
+    public int getNumPersonajes(){
+        
+        return listaPersonajes.size();
+    }
+    public Personaje buscarNombre(String nombre){
+        Personaje aux = new Personaje(nombre, "", "", "", 0, 0, 0, 0);
+        int indice = listaPersonajes.indexOf(aux);
+        if(indice>=0){
+            return listaPersonajes.get(indice);
+        }
+        return null;
+    }
+    public Personaje buscarRaza(String raza){
+        Personaje aux = new Personaje("", raza, "", "", 0, 0, 0, 0);
+        int indice = listaPersonajes.indexOf(aux);
+        if(indice>=0){
+            return listaPersonajes.get(indice);
+        }
+        return null;
+    }
+    public Personaje buscarArquetipo(String arquetipo){
+        Personaje aux = new Personaje("", "", arquetipo, "", 0, 0, 0, 0);
+        int indice = listaPersonajes.indexOf(aux);
+        if(indice>=0){
+            return listaPersonajes.get(indice);
+        }
+        return null;
+    }
+    public ArrayList<Personaje> buscarNombres(String nombre){
+        ArrayList<Personaje> aux = new ArrayList<>();
+        
+        for (Personaje pj : this.listaPersonajes) {
+            if (pj.getNombre().equalsIgnoreCase(nombre)){
+                aux.add(pj);
+            }
+        }
+        return aux;
+            
+        
+    }
     
 }
