b5231f8555a9917bdb50c0c7d389ebd220c93b22
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-02-09 14:32:06 +0100

    Cambio de nombre lista

--- src/main/java/canciones/PruebaReproduccion.java
+++ src/main/java/canciones/PruebaReproduccion.java
@@ -4,14 +4,13 @@ public class PruebaReproduccion {
 
     public static void main(String[] args) {
 
-        ListaReproduccion lista1 = new ListaReproduccion();
+        ListaReproduccion listaNueva = new ListaReproduccion();
         
-        lista1.grabarCancion(new Cancion("Muerte a pellizcos", "Vico", "Ranchera", 1, 2022));
-        lista1.grabarCancion(new Cancion("Ctrl+Alt+Supr", "Reno Renardo", "Rock", 1, 2013));
-        lista1.grabarCancion(new Cancion("Forget and Forgive", "Swan Fyahbwoy", "Rap", 4, 2018));
-        lista1.grabarCancion(new Cancion("Guapa", "El Chojin", "Rap", 3, 2015));
+        listaNueva.grabarCancion(new Cancion("Muerte a pellizcos", "Vico", "Ranchera", 1, 2022));
+        listaNueva.grabarCancion(new Cancion("Ctrl+Alt+Supr", "Reno Renardo", "Rock", 1, 2013));
+        listaNueva.grabarCancion(new Cancion("Forget and Forgive", "Swan Fyahbwoy", "Rap", 4, 2018));
+        listaNueva.grabarCancion(new Cancion("Guapa", "El Chojin", "Rap", 3, 2015));
         
-        System.out.println(lista1.getNumCanciones());
         
     }
 
