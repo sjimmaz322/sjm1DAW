f9d31b25b29213061260dd025de4e782cd43be98
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-16 13:57:52 +0100

    Muchos cambios, ninguno hace lo que quiero

--- src/main/java/pokino/PruebasPokino.java
+++ src/main/java/pokino/PruebasPokino.java
@@ -1,5 +1,7 @@
 package pokino;
 
+
+import java.util.TreeSet;
 /*
  * @author samjimmaz
  */
@@ -9,12 +11,51 @@ public class PruebasPokino {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Carton carton1 = new Carton();
-        
-        Naipe n1 = new Naipe();
 
-        System.out.println(n1);
-        
+        Naipe[][] carton1 = new Naipe[5][5];
+        boolean[][] estado = new boolean[5][5];
+        TreeSet<Naipe> naipes = new TreeSet<Naipe>();
+
+        for (int i = 0; i < 52; i++) {
+            Naipe n = new Naipe();
+            naipes.add(n);
+        }
     }
-    
+
+    public static void mostrarCarton(Naipe[][] matriz) {
+
+        System.out.println("|" + matriz[0][0] + "|" + matriz[0][1] + "|" + matriz[0][2] + "|" + matriz[0][3] + "|" + matriz[0][4] + "|");
+        System.out.println("|" + matriz[1][0] + "|" + matriz[1][1] + "|" + matriz[1][2] + "|" + matriz[1][3] + "|" + matriz[1][4] + "|");
+        System.out.println("|" + matriz[2][0] + "|" + matriz[2][1] + "|" + matriz[2][2] + "|" + matriz[2][3] + "|" + matriz[2][4] + "|");
+        System.out.println("|" + matriz[3][0] + "|" + matriz[3][1] + "|" + matriz[3][2] + "|" + matriz[3][3] + "|" + matriz[3][4] + "|");
+        System.out.println("|" + matriz[4][0] + "|" + matriz[4][1] + "|" + matriz[4][2] + "|" + matriz[4][3] + "|" + matriz[4][4] + "|");
+
+    }
+
+    public static void estadoCarton(boolean[][] matriz) {
+
+        System.out.println("|" + matriz[0][0] + "|" + matriz[0][1] + "|" + matriz[0][2] + "|" + matriz[0][3] + "|" + matriz[0][4] + "|");
+        System.out.println("|" + matriz[1][0] + "|" + matriz[1][1] + "|" + matriz[1][2] + "|" + matriz[1][3] + "|" + matriz[1][4] + "|");
+        System.out.println("|" + matriz[2][0] + "|" + matriz[2][1] + "|" + matriz[2][2] + "|" + matriz[2][3] + "|" + matriz[2][4] + "|");
+        System.out.println("|" + matriz[3][0] + "|" + matriz[3][1] + "|" + matriz[3][2] + "|" + matriz[3][3] + "|" + matriz[3][4] + "|");
+        System.out.println("|" + matriz[4][0] + "|" + matriz[4][1] + "|" + matriz[4][2] + "|" + matriz[4][3] + "|" + matriz[4][4] + "|");
+
+    }
+
+    public static void rellenarCarton(Naipe[][] aux) {
+        for (int i = 0; i < aux.length; i++) {
+            for (int j = 0; j < aux[i].length; j++) {
+                Naipe n = new Naipe();
+                aux[i][j] = n;
+            }
+        }
+    }
+
+    public static void limpiarCarton(boolean[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = false;
+            }
+        }
+    }
 }
