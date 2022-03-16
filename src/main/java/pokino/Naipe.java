fb93977d6aa89682a8fca36e9df36b7eb42d4e83
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-16 13:57:37 +0100

    Añadido equals y hash

--- src/main/java/pokino/Naipe.java
+++ src/main/java/pokino/Naipe.java
@@ -1,5 +1,6 @@
 package pokino;
 
+import java.util.Objects;
 import java.util.Random;
 
 /*
@@ -27,7 +28,34 @@ public class Naipe {
     @Override
     public String toString() {
         
-        return "El naipe es: "+valorCarta.getTipo()+" de "+paloCarta.getTipo();
+        return valorCarta.getTipo()+" de "+paloCarta.getTipo();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + Objects.hashCode(this.rd);
+        hash = 11 * hash + Objects.hashCode(this.paloCarta);
+        hash = 11 * hash + Objects.hashCode(this.valorCarta);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Naipe other = (Naipe) obj;
+        if (this.paloCarta != other.paloCarta) {
+            return false;
+        }
+        return this.valorCarta == other.valorCarta;
     }
     
     
