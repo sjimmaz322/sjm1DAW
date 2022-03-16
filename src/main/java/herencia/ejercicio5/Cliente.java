cfaac0bddbe612228baabf77c29d62ae6a419896
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-16 14:41:20 +0100

    Renamed and moved

--- src/main/java/herencia/ejercicio5/Cliente.java
+++ src/main/java/herencia/ejercicio5/Cliente.java
@@ -1,9 +1,11 @@
-package ejerciciosherencia.ejercicio5;
+package herencia.ejercicio5;
+
+import java.util.Objects;
 
 /*
  * @author samjimmaz
  */
-public class Ejercicio5 {
+public class Cliente {
 
     /*
     5.- Implementa la clase Cliente que hay en el apartado de “Sobrescritura del método equals” de los apuntes de clase.
@@ -13,8 +15,63 @@ public class Ejercicio5 {
     De los cuatro objetos creados con el parametrizado, 3 tendrán los mismos estados. 
     Imprime los hashCode de cada instancia, y comprueba cuáles son iguales y cuáles distintos.
     Comprobaciones de las propiedades transitiva, reflexiva, simétrica y nula.
-    */
+     */
+    private String nombre;
+    private String dni;
+    private int edad;
+
+    public Cliente() {
+        this.nombre = "NoName";
+        this.dni = "12345678Z";
+        this.edad = 18;
+    }
+
+    public Cliente(String nombre, String dni, int edad) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+    }
     
-    
-    
+
+    // Se omiten constructores, getters, setters
+    @Override
+    public int hashCode() {
+
+        int hash = 3;
+        hash = 59 * hash + Objects.hashCode(this.nombre);
+        hash = 59 * hash + Objects.hashCode(this.dni);
+        hash = 59 * hash + this.edad;
+        return hash;
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null) {
+            return false;
+        }
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Cliente other = (Cliente) obj;
+
+// Se comparan los estados de los objetos
+        if (this.edad != other.edad) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.dni, other.dni)) {
+            return false;
+        }
+        return true;
+    }
 }
