424a474ea43aa71293fd37f5ee8f737021ed4720
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-16 13:57:58 +0100

    Creada clase

---
+++ src/main/java/ejerciciosherencia/ejercicio5/Ejercicio5.java
@@ -0,0 +1,17 @@
+package ejerciciosherencia.ejercicio5;
+
+/*
+ * @author samjimmaz
+ */
+public class Ejercicio5 {
+
+    /*
+    5.- Implementa la clase Cliente que hay en el apartado de “Sobrescritura del método equals” de los apuntes de clase.
+    Incluye los constructores, getters, setters y toString. Sobrescribe también los métodos equals y hashCode.
+    Realiza una clase que incluya el método main() con el siguiente código:
+    Creación de 5 objetos Cliente, uno con el constructor por defecto y cuatro con el parametrizado.
+    De los cuatro objetos creados con el parametrizado, 3 tendrán los mismos estados. 
+    Imprime los hashCode de cada instancia, y comprueba cuáles son iguales y cuáles distintos.
+    Comprobaciones de las propiedades transitiva, reflexiva, simétrica y nula.
+    */
+}
