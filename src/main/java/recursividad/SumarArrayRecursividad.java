eff90f916294eb083aac874eb5762ac387ae79c8
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-17 10:37:43 +0100

    Creada clase y añadido ejemplo

---
+++ src/main/java/recursividad/SumarArrayRecursividad.java
@@ -0,0 +1,19 @@
+package recursividad;
+
+public class SumarArrayRecursividad {
+
+    public static void main(String[] args) {
+
+        int num[] = {7, 1, 2, 3, 4};
+        System.out.println(sumar(num, num.length));
+
+    }
+
+    private static int sumar(int[] array, int tamanio) {
+        if (tamanio == 1) {
+            return array[0];
+        } else {
+            return array[tamanio - 1] + sumar(array, tamanio - 1);
+        }
+    }
+}
