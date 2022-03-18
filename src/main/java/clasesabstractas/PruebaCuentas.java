a9dde53f3ecfa83d040496ca7417cfa755a45127
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-18 13:27:25 +0100

    Terminada clase, probados métodos, son funcionales

--- src/main/java/clasesabstractas/PruebaCuentas.java
+++ src/main/java/clasesabstractas/PruebaCuentas.java
@@ -26,6 +26,23 @@ public class PruebaCuentas {
         System.out.println("Retiramos cantidad incorrecta");
         cuco1.retirar(400);
         System.out.println(cuco1.toString());
+        
+//        CuentaAhorro cuah1 = new CuentaAhorro(interes, comisionanual, saldo, persona);
+
+        CuentaAhorro cuah1 = new CuentaAhorro(1.5, 12.5, 500, new Persona("Samuel", "Jiménez", "31014322H"));
+        System.out.println(cuah1.toString());
+        cuah1.actualizarSaldo();
+        System.out.println("Tras actualizar");
+        System.out.println(cuah1.toString());
+        System.out.println("Retiramos cantidad que deja saldo positivo");
+        cuah1.retirar(400);
+        System.out.println(cuah1.toString());
+        System.out.println("Retiramos cantidad que nos deja saldo negativo");
+        cuah1.retirar(200);
+        System.out.println(cuah1.toString());
+        cuah1.setComisionAnual(1000);
+        cuah1.actualizarSaldo();
+        System.out.println(cuah1.toString());
 
     }
 
