c3267ba7e03ff83ac25f2ccda5fa384ece4a456c
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-22 13:04:51 +0100

    Creada clase y puesta la búsqueda recursiva

---
+++ src/main/java/clasesabstractas/BusquedaRecursiva.java
@@ -0,0 +1,22 @@
+package clasesabstractas;
+
+public class BusquedaRecursiva {
+
+    public static void main(String[] args) {
+
+        //Búsqueda secuencial en una lista
+    }
+
+    public static int buscarElemento(int[] lista, int elemento, int i) {
+        if ((i == lista.length) || (lista[i] == elemento)) {
+            if (i == lista.length) {
+                return -1;
+            } else {
+                return i;
+            }
+        } else {
+            return buscarElemento(lista, elemento, i + 1);
+        }
+    }
+
+}
