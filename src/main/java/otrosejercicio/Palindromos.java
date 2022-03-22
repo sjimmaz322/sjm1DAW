ef4c5af4ef2783ca0050ed9a7da4027c86caefd1
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-22 14:43:26 +0100

    Iniciado recursividad, no funciona, a veces si, otras no, echarle un ojo

--- src/main/java/otrosejercicio/Palindromos.java
+++ src/main/java/otrosejercicio/Palindromos.java
@@ -5,22 +5,23 @@ import java.util.Scanner;
 
 public class Palindromos {
 
-    
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Mete una palabra y te dice si es un palíndromo o no
         System.out.println("Introduce la palabra que deseas comprobar");
         String palabra = sc.nextLine();
-        
-        boolean resultado = comprobarPalabra(palabra);
-        
-        String palin = resultado?"La palabra "+palabra+" es un palíndormo":"La palabra "+palabra+" no es un palíndormo";
-        
-        System.out.println(palin);
-    }
 
-  
+        boolean resultado = comprobarPalabra(palabra);
+        boolean resultadoRecur = comprobarPalabraRecur(palabra, 0);
+
+        String palin = resultado ? "La palabra " + palabra + " es un palíndormo" : "La palabra " + palabra + " no es un palíndormo";
+
+        System.out.println(palin);
+        
+        String palin2 = resultadoRecur ? "La palabra " + palabra + " es un palíndormo" : "La palabra " + palabra + " no es un palíndormo";
+
+        System.out.println(palin2);
+    }
 
     private static boolean comprobarPalabra(String palabra) {
         ArrayList list1 = new ArrayList();
@@ -29,9 +30,11 @@ public class Palindromos {
         for (int i = 0; i < palabra.length(); i++) {
             list1.add(palabra.charAt(i));
         }
-        for (int j = palabra.length()-1; j >= 0; j--) {
+        for (int j = palabra.length() - 1; j >= 0; j--) {
             list2.add(palabra.charAt(j));
         }
+
+//        Esto es para comprobar que las palabras se san la vuelta en el segundo caso
 //        for (int k = 0; k < list1.size(); k++) {
 //            System.out.print(list1.get(k));
 //        }
@@ -39,14 +42,33 @@ public class Palindromos {
 //        for (int m = 0; m < list2.size(); m++) {
 //            System.out.print(list2.get(m));
 //        }
-
         for (int k = 0; k < list1.size(); k++) {
-            if (!(list1.get(k).equals(list2.get(k)))){
+            if (!(list1.get(k).equals(list2.get(k)))) {
                 return false;
             }
-                
-            }
-        return true;
-        }
-    }
 
+        }
+        return true;
+    }
+    private static boolean comprobarPalabraRecur(String palabra, int pos) {
+        ArrayList list3 = new ArrayList();
+        ArrayList list4 = new ArrayList();
+        
+
+        for (int i = 0; i < palabra.length(); i++) {
+            list3.add(palabra.charAt(i));
+        }
+        for (int j = palabra.length() - 1; j >= 0; j--) {
+            list4.add(palabra.charAt(j));
+        }
+        if (!(list3.get(pos).equals(list4.get(pos)))){
+            return false;
+        }else{
+            if(pos<palabra.length()){
+            comprobarPalabraRecur(palabra, pos+1);             
+            }
+            return true;
+        }
+        
+    }
+}
