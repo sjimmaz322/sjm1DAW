8aebbc00536ff9bf7b438e2342f317f6a2955841
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-25 13:30:53 +0100

    Creadas clases interfaces

---
+++ src/main/java/interfaces/Panel.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaces;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author samjimmaz
+ */
+public class Panel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Construimos la ventana
+        JFrame ventanaPrincipal = new JFrame("Aplicación");
+
+        // Establecemos tamaño y posición
+        ventanaPrincipal.setSize(800, 600);
+        ventanaPrincipal.setLocationRelativeTo(null);
+
+        // Añadimos un objeto MiPanel al JFrame
+        ventanaPrincipal.add(new MiPanel(Color.red));
+
+        // Hacemos visible la ventana
+        ventanaPrincipal.setVisible(true);
+
+        // Acción por defecto al pulsar el botón de cierre de la ventana
+        ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+}
+
+
---
+++ src/main/java/interfaces/VentanaSimple.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package interfaces;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author samjimmaz
+ */
+public class VentanaSimple {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Construimos la ventana
+	JFrame ventanaPrincipal = new JFrame ("Aplicación");
+
+	// Establecemos posición y tamaño
+	ventanaPrincipal.setBounds(250, 250, 800, 600);
+
+	// Hacemos visible la ventana
+	ventanaPrincipal.setVisible(true);
+
+// Acción por defecto al pulsar el botón de cierre 	 ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    
+}
