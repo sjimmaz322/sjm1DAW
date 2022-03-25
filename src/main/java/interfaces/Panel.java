d6a8f9d79cd78d3f0ffa2adf2c7e5c65d830de70
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-25 13:45:42 +0100

    CD

--- src/main/java/interfaces/Panel.java
+++ src/main/java/interfaces/Panel.java
@@ -4,6 +4,7 @@
  */
 package interfaces;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JFrame;
 
@@ -32,8 +33,23 @@ public class Panel {
 
         // Acción por defecto al pulsar el botón de cierre de la ventana
         ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ventanaPrincipal.pack();
+        // Añadir panel amarillo en la posición superior (NORTH)
+        ventanaPrincipal.add(new MiPanel(Color.orange), BorderLayout.NORTH);
+
+        // Añadir panel rojo en la posición inferior (SOUTH)
+        ventanaPrincipal.add(new MiPanel(Color.pink), BorderLayout.SOUTH);
+
+// Añadir panel gris en la posición central (CENTER)
+        ventanaPrincipal.add(new MiPanel(Color.magenta), BorderLayout.CENTER);
+
+// Añadir panel verde en la posición izquierda (WEST)
+        ventanaPrincipal.add(new MiPanel(Color.white), BorderLayout.WEST);
+
+// Añadir panel cián en la posición derecha (EAST)
+        ventanaPrincipal.add(new MiPanel(Color.blue), BorderLayout.EAST);
+
     }
 
 }
-
-
