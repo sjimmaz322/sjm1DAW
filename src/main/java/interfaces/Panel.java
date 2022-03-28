f3fd95face9442aa7c2ee8d737169c132aa311ba
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-03-28 21:43:49 +0200

    Moved and copied

--- src/main/java/interfaces/Panel.java
+++ src/main/java/interfaces/Panel.java
@@ -1,8 +1,10 @@
+package practicapanel;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package interfaces;
+
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -18,15 +20,11 @@ public class Panel {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Construimos la ventana
-        JFrame ventanaPrincipal = new JFrame("Aplicación");
+      JFrame ventanaPrincipal = new JFrame("Samuel Jiménez Mazas");
 
         // Establecemos tamaño y posición
         ventanaPrincipal.setSize(800, 600);
         ventanaPrincipal.setLocationRelativeTo(null);
-
-        // Añadimos un objeto MiPanel al JFrame
-        ventanaPrincipal.add(new MiPanel(Color.LIGHT_GRAY));
 
         // Hacemos visible la ventana
         ventanaPrincipal.setVisible(true);
@@ -34,21 +32,32 @@ public class Panel {
         // Acción por defecto al pulsar el botón de cierre de la ventana
         ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        ventanaPrincipal.pack();
-        // Añadir panel amarillo en la posición superior (NORTH)
-        ventanaPrincipal.add(new MiPanel(Color.orange), BorderLayout.NORTH);
+        //Establecemos el layout del JFrame
+        ventanaPrincipal.setLayout(new BorderLayout());
 
-        // Añadir panel rojo en la posición inferior (SOUTH)
-        ventanaPrincipal.add(new MiPanel(Color.pink), BorderLayout.SOUTH);
+        //La ventana no se puede redimensionar
+        ventanaPrincipal.setResizable(false);
 
-// Añadir panel gris en la posición central (CENTER)
-        ventanaPrincipal.add(new MiPanel(Color.magenta), BorderLayout.CENTER);
+        //Creamos los 5 paneles y los añadimos a la ventana
+        MiPanel verde = new MiPanel(Color.CYAN);
+        verde.getEtiqueta().setText("Panel Color Cián");
+        ventanaPrincipal.add(verde, BorderLayout.SOUTH);
 
-// Añadir panel verde en la posición izquierda (WEST)
-        ventanaPrincipal.add(new MiPanel(Color.white), BorderLayout.WEST);
+        MiPanel amarillo = new MiPanel(Color.LIGHT_GRAY);
+        amarillo.getEtiqueta().setText("Panel Color Gris claro");
+        ventanaPrincipal.add(amarillo, BorderLayout.EAST);
 
-// Añadir panel cián en la posición derecha (EAST)
-        ventanaPrincipal.add(new MiPanel(Color.blue), BorderLayout.EAST);
+        MiPanel magenta = new MiPanel(Color.RED);
+        magenta.getEtiqueta().setText("Panel Color Rojo");
+        ventanaPrincipal.add(magenta, BorderLayout.WEST);
+
+        MiPanel naranja = new MiPanel(Color.pink);
+        naranja.getEtiqueta().setText("Panel Color Rosa");
+        ventanaPrincipal.add(naranja, BorderLayout.NORTH);
+
+        MiPanel azul = new MiPanel(Color.GREEN);
+        azul.getEtiqueta().setText("Panel Color Verde");
+        ventanaPrincipal.add(azul, BorderLayout.CENTER);
 
     }
 
--- src/main/java/interfaces/VentanaSimple.java
+++ src/main/java/interfaces/VentanaSimple.java
@@ -1,8 +1,10 @@
+package practicapanel;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package interfaces;
+
 
 import javax.swing.JFrame;
 
