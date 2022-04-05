e0b9d03285028af8c2fbe790436e79266f6bbdb7
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-04-05 10:34:25 +0200

    Nada funciona, todo mal, menua mierda

--- src/main/java/ejerciciovuelo/Aeropuerto.java
+++ src/main/java/ejerciciovuelo/Aeropuerto.java
@@ -9,29 +9,36 @@ public class Aeropuerto {
     public static void main(String[] args) {
 
         ArrayList<Vuelo> registro = new ArrayList<>();
-        Map<String, ArrayList<Pasajero>> lista = new HashMap<String, ArrayList<Pasajero>>();
+        Map<String, ArrayList<Pasajero>> lista = new HashMap<>();
 
-        Vuelo v1 = new Vuelo("V001", "Albacete", "Noruega", "37 horas", new ArrayList<Pasajero>());
-        v1.getUsuarios().add(new Pasajero("Turista", "Samuel", "Jiménez", new Nif("31014322")));
+        Vuelo v1 = new Vuelo("V001", "Albacete", "Noruega", "37 horas", new ArrayList<>());
+        v1.getUsuarios().add(new Pasajero("Turista", "P1", "p1", new Nif("001")));
+        v1.getUsuarios().add(new Pasajero("Turista", "P2", "p2", new Nif("002")));
+        v1.getUsuarios().add(new Pasajero("Turista", "P3", "p3", new Nif("003")));
+        v1.getUsuarios().add(new Pasajero("Turista", "P4", "p4", new Nif("004")));
+        v1.getUsuarios().add(new Pasajero("Turista", "P5", "p5", new Nif("005")));
 
-
+//        System.out.println(Aeropuerto.getPasajeros("V001",registro));
+//        lista = Aeropuerto.mapeado(registro);
+               
     }
 
-   private static Map<Vuelo, Integer> mapeado(ArrayList<Vuelo> listaVuelos) {
-        Map<Vuelo, Integer> listaPasajeros = new HashMap<>();
-        for (int i = 0; i < listaVuelos.size(); i++) {
-
-        }
-        return listaPasajeros;
-    }
-
-    private static int mapeado(String codigo, ArrayList<Vuelo> listaPasajeros) {
-        int numPasajeros = 0;
-        for (int i = 0; i < listaPasajeros.size(); i++) {
-            if (listaPasajeros.get(i).equals(codigo)) {
-                numPasajeros = listaPasajeros.get(i).getUsuarios().size();
-            }
-        }
-        return numPasajeros;
-    }
+//   private static Map<Vuelo, Integer> mapeado(ArrayList<Vuelo> listaVuelos) {
+//        Map<Vuelo, Integer> listaPasajeros = new HashMap<>();
+//        for (int i = 0; i < listaVuelos.size(); i++) {
+//
+//        }
+//        return listaPasajeros;
+//    }
+//
+//        private static int getPasajeros(String codigo, ArrayList<Vuelo> listaVuelos) {
+//
+//        for (int i = 0; i < listaVuelos.size(); i++) {
+//            if (listaVuelos.get(i).getCodVuelo().equalsIgnoreCase(codigo)) {
+//                return listaVuelos.get(i).getUsuarios().size();
+//            }
+//
+//        }
+//        return 0;
+//    }
 }
