2a23bb28722bbf7b8540a93f2fd812db91b55e5f
Sjimmaz322 <sjimmaz322@g.educaand.es>
2022-04-20 14:37:09 +0200

    A ver si lo arreglo

---
+++ src/main/java/archivos/Ejercicio6.java
@@ -0,0 +1,47 @@
+package archivos;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/*
+Implementa un programa que lea el fichero del ejercicio 3 y muestre el contenido del fichero.
+Además buscará la primera ocurrencia, en cada línea, de las letras seguidas ‘w’, ‘e’, ‘b’, 
+informando de la posición que ocupan en la línea, si es que existe esa ocurrencia.
+ */
+public class Ejercicio6 {
+
+    public static void main(String[] args) {
+
+        String idFichero = "lineas.txt";
+        String linea;
+        String[] tokens;
+
+        int pos = 0;
+
+        try ( Scanner datosFichero = new Scanner(new File(idFichero), "UTF-8")) {
+            // hasNextLine devuelve true mientras haya líneas por leer
+            while (datosFichero.hasNextLine()) {
+                // Guarda la línea completa en un String
+                linea = datosFichero.nextLine();
+                // Se guarda en el array de String cada elemento de la
+                // línea en función del carácter separador de campos del fichero CSV
+                tokens = linea.split(";");
+
+                for (String string : tokens) {
+                    System.out.print(string);
+                if(string.toLowerCase().contains("web")){
+                    pos = string.indexOf("web");
+                }
+                }
+                System.out.println();
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Está en la posición " + pos);
+
+    }
+
+}
