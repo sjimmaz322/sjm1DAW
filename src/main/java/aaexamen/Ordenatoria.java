891d708f9c948a48c3ae871ef1c43c3828c1465e
sjimmaz322 <sjimmaz322@g.educaand.es>
2022-05-11 23:44:04 +0200

    Para mañana

---
+++ src/main/java/aaexamen/Ordenatoria.java
@@ -0,0 +1,11 @@
+package aaexamen;
+
+import java.util.Comparator;
+
+public class Ordenatoria {
+
+    public static void ordenarPorX() {
+      //  Comparator<Object> ordenarX = (o1, o2) -> Double.compare(o1.getAlgo(), o2.getAlgo());
+    }
+
+}
